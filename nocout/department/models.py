99cba6f4dc42ad7a64cd4b6015e3a51c1bdf4d23
nocout Dev <nodev@ubuntu.(none)>
2014-04-22 13:40:21 +0530

    organization and departements are put in the user_group.models and user.models respectively. to be saved from circular dependency

--- nocout/department/models.py
+++ nocout/department/models.py
@@ -1,12 +1,3 @@
-# Create your models here.
 from django.db import models
-from user_group.models import UserGroup
-from user_profile.models import UserProfile
-class Department(models.Model):
-	User_group =models.ForeignKey(UserGroup)
-	User_profile =models.ForeignKey(UserProfile)
-	
 
-
-
-
+#@TODO: the organization exists in the user models
