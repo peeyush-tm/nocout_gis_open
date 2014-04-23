375017b2c2e091725386d9dab14fb240b1b58dee
Priyesh <priyesh@teramatrix.co>
2014-04-23 15:39:23 +0530

    Modify 'settings.py' & 'urls.py'

--- nocout/nocout/urls.py
+++ nocout/nocout/urls.py
@@ -3,8 +3,8 @@ from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 dajaxice_autodiscover()
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'home.views.home'),
@@ -26,5 +26,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
