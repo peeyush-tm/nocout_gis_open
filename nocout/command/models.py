de44ef0091c160d21c064eaae9b8393230655233
Priyesh <priyesh@teramatrix.co>
2014-04-24 16:22:56 +0530

    Create command create, update, delete functionality.

--- nocout/command/models.py
+++ nocout/command/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Command(models.Model):
-    command_name = models.CharField(max_length=100)
+    command_name = models.CharField(max_length=100, unique=True)
     command_line = models.CharField(max_length=100)
     
