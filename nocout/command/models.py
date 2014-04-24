53427b888762f75223b5c7b79d01375d93d34726
Priyesh <priyesh@teramatrix.co>
2014-04-24 18:11:41 +0530

    Modify command model.py for making existing fields null & blank.

--- nocout/command/models.py
+++ nocout/command/models.py
@@ -4,3 +4,6 @@ class Command(models.Model):
     command_name = models.CharField(max_length=100, unique=True)
     command_line = models.CharField(max_length=100)
     
+    def __unicode__(self):
+        return self.command_name
+    
