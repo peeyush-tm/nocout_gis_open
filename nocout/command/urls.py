4f5c722d7371fae57bee596af081124e10a6dc1f
Priyesh <priyesh@teramatrix.co>
2014-04-24 16:24:59 +0530

    Complete command app functionality.

---
+++ nocout/command/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+from command import views
+
+urlpatterns = patterns('',
+  url(r'^$', views.CommandList.as_view(), name='commands_list'),
+  url(r'^(?P<pk>\d+)$', views.CommandDetail.as_view(), name='command_detail'),
+  url(r'^new/$', views.CommandCreate.as_view(), name='command_new'),
+  url(r'^edit/(?P<pk>\d+)$', views.CommandUpdate.as_view(), name='command_edit'),
+  url(r'^delete/(?P<pk>\d+)$', views.CommandDelete.as_view(), name='command_delete'),
+)
