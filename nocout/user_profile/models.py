39a810131b28ea614b93382bf330f2c1d722748c
Priyesh <priyesh@teramatrix.co>
2014-04-29 15:21:28 +0530

    Adding roles table in 'user_profile'.

--- nocout/user_profile/models.py
+++ nocout/user_profile/models.py
@@ -3,17 +3,18 @@ from django.contrib.auth.models import User, UserManager
 from user_group.models import UserGroup
 from mptt.models import MPTTModel, TreeForeignKey
 
+
+# user profile class
 class UserProfile(MPTTModel, User):
     admin = 'Admin'
     operator = 'Operator'
     viewer = 'Viewer'
     ROLES = (
-        (admin, 'Admin'),
         (operator, 'Operator'),
         (viewer, 'Viewer')
     )
     parent = TreeForeignKey('self', null=True, blank=True, related_name='user_children')
-    role = models.CharField('Role', max_length=50, choices=ROLES, default=admin )
+    role = models.ManyToManyField('Roles', null=True, blank=True)
     user_group = models.ManyToManyField(UserGroup, through='Department', null=True, blank=True)
     user_group.help_text = ''
     phone_number = models.CharField('Phone No.', max_length=15, null=True, blank=True)
@@ -25,6 +26,17 @@ class UserProfile(MPTTModel, User):
     # Use UserManager to get the create_user method, etc.
     objects = UserManager()
 
+<<<<<<< Updated upstream
+=======
+
+# user roles class
+class Roles(models.Model):
+    role_name = models.CharField('Role Name', max_length=100, null=True, blank=True)
+    role_description = models.CharField('Role Description', max_length=250, null=True, blank=True)
+
+
+# user_profile & user_group relationship class
+>>>>>>> Stashed changes
 class Department(models.Model):
     user_profile = models.ForeignKey(UserProfile)
     user_group = models.ForeignKey(UserGroup)
