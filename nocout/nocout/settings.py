6d5cc9483546244e1069038b3eab2c4eaa6c3f18
Peeyush Raj <praj@teramatrix.in>
2014-05-12 20:34:09 +0530

    updated settings.py file so as to read all the 'static' folder files per app basis  and compile correctly to the command collectstatic

--- nocout/nocout/settings.py
+++ nocout/nocout/settings.py
@@ -77,7 +77,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -85,8 +85,9 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, "static"),
+    os.path.join(PROJECT_DIR, "static/"),
     )
+
 
 # List of finder classes that know how to find static files in
 # various locations.
