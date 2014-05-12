859f60993ae6799219ad73e3dddd811e2c79b9ac
nishant <nishant@teramatrix.co>
2014-05-12 19:09:51 +0530

    [#71135630]LoginRequiredMiddleware to implement Default Login page for any url if user is not logged in.

--- nocout/nocout/middlewares/LoginRequiredMiddleware.py
+++ nocout/nocout/middlewares/LoginRequiredMiddleware.py
@@ -1,1 +1,18 @@
-__author__ = 'nishant'
+from django.http import HttpResponseRedirect
+from django.conf import settings
+import sys
+
+
+class LoginRequiredMiddleware:
+
+    def process_request(self, request):
+        # Required to debug-------------------
+        # print str(request.user) +' in LoginRequiredMiddleware '+sys._getframe().f_code.co_name
+        # print 'printint path %s'%str((request.path_info.lstrip('/')))
+        #--------------------------
+        if not request.user.is_authenticated():
+            path = request.path_info.lstrip('/')
+            if not(path in settings.LOGIN_EXEMPT_URLS):
+                return HttpResponseRedirect(settings.LOGIN_URL)
+
+
