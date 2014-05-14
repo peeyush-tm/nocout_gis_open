29e9fc1941a95ae3b25dd01a40104f9c36ecd097
Pardeep Singh <pardeep.singh@teramatrix.co>
2014-05-14 11:49:34 +0530

    Revert "result dict in-line comment"
    This reverts commit 590c0177f5dbcab84d048189fe3649900cd79d72.

--- nocout/device/api.py
+++ nocout/device/api.py
@@ -41,7 +41,6 @@ class DeviceStatsApi(View):
             username = request.user.username
         host_ip = request.get_host().split(':')[0]
         show_link = 1
-        #The result prototype dict
         self.result = {
             "success": 0,
             "message": "No Device Data",
