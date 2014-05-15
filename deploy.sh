4b706cd767de14fa5efd7f92ef8b48d3d341fe70
Peeyush Raj <praj@teramatrix.in>
2014-05-15 14:35:37 +0530

    deployment using the supervisord

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-cd /opt/nocout/nocout_gis/ && source /opt/nocout/nout/bin/activate && git pull origin dev_master && cd nocout && python manage.py collectstatic -c --noinput && pkill -HUP uwsgi && cd /opt/nocout/nocout_gis/ && deactivate
+without supervisor:
+	cd /opt/nocout/nocout_gis/ && source /opt/nocout/nout/bin/activate && git pull origin dev_master && cd nocout && python manage.py collectstatic -c --noinput && pkill -HUP uwsgi && cd /opt/nocout/nocout_gis/ && deactivate
+
+with supervisor:
+	cd /opt/nocout/nocout_gis/ && source /opt/nocout/nout/bin/activate && git pull origin dev_master && cd nocout && python manage.py collectstatic -c --noinput && sudo supervisorctl restart nocout-uwsgi && cd /opt/nocout/nocout_gis/ && deactivate
