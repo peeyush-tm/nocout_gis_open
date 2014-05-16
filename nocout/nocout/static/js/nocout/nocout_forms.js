1e5260b143bfa90508b90659bda3e1df8a6eb490
Priyesh <priyesh@teramatrix.co>
2014-05-16 16:30:46 +0530

    Improve 'nocout' static folder structure.

---
+++ nocout/nocout/static/js/nocout/nocout_forms.js
@@ -0,0 +1,29 @@
+$(document).ready(function (e) {
+
+    var inputContainers = $(".vendorFromContainer .col-sm-9");
+    for (var i = 0; i < inputContainers.length; i++) {
+        var childrenCount = inputContainers[i].children.length;
+        if (childrenCount == 2) {
+            var errCLass = $.trim(inputContainers[i].children[1].className);
+            if (errCLass == "errorlist") {
+                var formGroup = inputContainers[i].parentElement;
+                formGroup.className = formGroup.className + " has-error";
+            }
+        }
+    }
+});
+/*It removes the error class from fields if exists*/
+function resetForm() {
+    var inputContainers = $(".vendorFromContainer .col-sm-9");
+    for (var i = 0; i < inputContainers.length; i++) {
+        var childrenCount = inputContainers[i].children.length;
+        if (childrenCount == 2) {
+            var errCLass = $.trim(inputContainers[i].children[1].className);
+
+            if (errCLass == "errorlist") {
+                inputContainers[i].children[1].remove();
+                inputContainers[i].parentElement.className = "form-group";
+            }
+        }
+    }
+}
