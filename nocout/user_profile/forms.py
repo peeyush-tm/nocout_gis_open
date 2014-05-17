b65baae771af69f7352e409b9a25fe70eb6efa2f
Priyesh <priyesh@teramatrix.co>
2014-05-18 01:25:17 +0530

    User create & update 'forms' are modified.

--- nocout/user_profile/forms.py
+++ nocout/user_profile/forms.py
@@ -6,6 +6,11 @@ from nocout.widgets import MultipleToSingleSelectionWidget
 class UserForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            if field.widget.attrs.has_key('class'):
+                field.widget.attrs['class'] += ' form-control'
+            else:
+                field.widget.attrs.update({'class':'form-control'})
         self.base_fields['user_group'].help_text = "Please select a Usergroup."
 
     class Meta:
