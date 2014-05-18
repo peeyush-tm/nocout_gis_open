8286d82b3b927fc27aee5ad5efd10023e6f73086
yogender-tm <yogender@teramatrix.co>
2014-05-18 11:55:44 +0530

    Pass the username in the template

--- nocout/devicevisualization/views.py
+++ nocout/devicevisualization/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render_to_response
 
 def locate_devices(request):
-    first_name = request.user.first_name
-    last_name = request.user.last_name
-    c = {}
-    host_ip = request.get_host().split(':')[0]
-    c.update({"host_ip": host_ip, "firstname": first_name, "lastname": last_name})
-    return render_to_response('devicevisualization/locate_devices.html', c)
+    context = {}
+    context.update({
+    	"host_ip": request.get_host().split(':')[0],
+    	"firstname": request.user.first_name,
+    	"lastname": request.user.last_name,
+    	"username": request.user.username
+    })
+    return render_to_response('devicevisualization/locate_devices.html', context)
