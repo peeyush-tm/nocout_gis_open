095992c7ec07b3541fdc5aa623fe03fd4a44ea59
Peeyush Raj <praj@teramatrix.in>
2014-05-21 21:47:36 +0530

    Proper use of context variable implemented in devicevisualization/views.py

--- nocout/devicevisualization/views.py
+++ nocout/devicevisualization/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render_to_response
 from user_profile.models import UserProfile
 from user_group.models import Organization
+from django.template import RequestContext
 
 
 def locate_devices(request):
 
     username = request.user.username
     
-    c = {}
+    template_data = { 'host_info' : 
+    					request.META['SERVER_NAME'] + ":" + \
+    					request.META['SERVER_PORT'] + "/",
+    				'username' : request.user.username
+    				}
 
-    host_info = request.META['SERVER_NAME'] + ":" +\
-                request.META['SERVER_PORT'] + "/"
-
-    c.update({"host_info": host_info,
-                "username": username
-        })
-
-    return render_to_response('devicevisualization/locate_devices.html', c)
+    return render_to_response('devicevisualization/locate_devices.html', 
+    							template_data, 
+    							context_instance=RequestContext(request))
