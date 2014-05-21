7094f5c22f1ffcaa62122a9a70d3b59c3d69c3f3
Peeyush Raj <praj@teramatrix.in>
2014-05-21 21:54:23 +0530

    for getting server information use  object and META information

--- nocout/devicevisualization/views.py
+++ nocout/devicevisualization/views.py
@@ -9,11 +9,11 @@ def locate_devices(request):
     username = request.user.username
     
     template_data = { 'host_info' : 
-    					request.META['SERVER_NAME'] + ":" + \
-    					request.META['SERVER_PORT'] + "/",
-    				'username' : request.user.username
-    				}
+                        request.META['SERVER_NAME'] + ":" + \
+                        request.META['SERVER_PORT'] + "/",
+                    'username' : request.user.username
+                    }
 
     return render_to_response('devicevisualization/locate_devices.html', 
-    							template_data, 
-    							context_instance=RequestContext(request))
+                                template_data, 
+                                context_instance=RequestContext(request))
