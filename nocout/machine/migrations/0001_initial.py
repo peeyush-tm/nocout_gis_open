d2f073cc3a34e177347ad2ca629be2ec242012c4
Priyesh <priyesh@teramatrix.co>
2014-06-03 19:38:48 +0530

    Modify machine 'app' to include 'Machine' model.

--- nocout/machine/migrations/0001_initial.py
+++ nocout/machine/migrations/0001_initial.py
@@ -8,32 +8,32 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'MachineInstance'
-        db.create_table(u'machine_machineinstance', (
+        # Adding model 'Machine'
+        db.create_table(u'machine_machine', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
             ('alias', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('machine_ip', self.gf('django.db.models.fields.IPAddressField')(max_length=15, null=True, blank=True)),
             ('agent_port', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
         ))
-        db.send_create_signal(u'machine', ['MachineInstance'])
+        db.send_create_signal(u'machine', ['Machine'])
 
 
     def backwards(self, orm):
-        # Deleting model 'MachineInstance'
-        db.delete_table(u'machine_machineinstance')
+        # Deleting model 'Machine'
+        db.delete_table(u'machine_machine')
 
 
     models = {
-        u'machine.machineinstance': {
-            'Meta': {'object_name': 'MachineInstance'},
+        u'machine.machine': {
+            'Meta': {'object_name': 'Machine'},
             'agent_port': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
             'alias': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'description': ('django.db.models.fields.TextField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'machine_ip': ('django.db.models.fields.IPAddressField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
         }
     }
 
