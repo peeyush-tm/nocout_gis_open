87b51fe04e3b847ef36c97cb4082c0b210891939
yogender-tm <yogender@teramatrix.co>
2014-06-03 14:10:35 +0530

    Add code as per the hiperlink on device tree view

--- nocout/devicevisualization/urls.py
+++ nocout/devicevisualization/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url, include
 from devicevisualization import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.locate_devices),		
+	url(r'^$', views.locate_devices),
+	url(r'^(?P<device_name>\w+)/$', views.locate_devices),
 )
