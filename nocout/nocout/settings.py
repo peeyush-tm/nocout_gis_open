d8829b1e6c276bd8dc30d116903cc58ed4d2e449
nishant <nishant@teramatrix.co>
2014-06-03 17:29:04 +0530

    Adding activity_stream in the settings.py and the user_profile.organization in the ACTSTREAM_MODEL Setting.

--- nocout/nocout/settings.py
+++ nocout/nocout/settings.py
@@ -171,7 +171,7 @@ INSTALLED_APPS = (
     'preventconcurrentlogins',
     'corsheaders',
     'actstream',
-
+    'activity_stream',
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
@@ -230,7 +230,7 @@ ACTSTREAM_SETTINGS = {
     'MODELS': ('auth.user', 'auth.group', 'sites.site', 'comments.comment','user_profile.userprofile', 'user_group.usergroup',
                 'device.device','device_group.devicegroup','device.devicetypefields','device.devicetechnology',
                 'device.devicevendor','device.devicemodel','device.devicetype','site_instance.siteinstance','service.service',
-                'service.serviceparameters','command.command','organization.organization'),
+                'service.serviceparameters','command.command','user_profile.organization',),
     'MANAGER': 'actstream.managers.ActionManager',
     'FETCH_RELATIONS': True,
     'USE_PREFETCH': True,
