2efd964963f8368033836151642e309005863227
Priyesh <priyesh@teramatrix.co>
2014-06-03 20:07:11 +0530

    Restructuring performance model.

--- nocout/performance/models.py
+++ nocout/performance/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from device.models import Device
-from service.models import Service
 
 class PerformanceMetric(models.Model):
-    device = models.ForeignKey(Device, null=True, blank=True)
-    service = models.ForeignKey(Service, null=True, blank=True)
-    timestamp = models.DateTimeField(null=True, blank=True)
-    value = models.CharField(max_length=500, null=True, blank=True)
+    device_name = models.CharField('Device Name', max_length=100)
+    service_name = models.CharField('Service Name', max_length=100)
+    machine_id = models.IntegerField('Machine ID')
+    site_id = models.IntegerField('Site ID')
+    data_source = models.CharField('Data Source', max_length=100)
+    value = models.CharField('Value', max_length=50)
+    sys_timestamp = models.DateTimeField('System Timestamp')
+    check_timestamp = models.DateTimeField('Check Timestamp')
