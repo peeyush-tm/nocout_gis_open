0fc732da60a4fcc4e3b00574371ec33db1a6c402
nishant <nishant@teramatrix.co>
2014-06-08 12:31:56 +0530

    [#68715252] Adding the MAX CONCURRENT USER constraint in the login.

--- nocout/nocout/views.py
+++ nocout/nocout/views.py
@@ -1,14 +1,12 @@
 import json
 from actstream import action
-from django.conf import settings
-from django.contrib.sessions.models import Session
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib import auth
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.models import User
+from nocout.settings import MAX_USER_LOGIN_LIMIT
 from session_management.models import Visitor
-from django.db.models.signals import pre_delete
 
 @csrf_protect
 def login(request):
@@ -16,6 +14,18 @@ def login(request):
 
 
 def auth_view(request):
+
+        if Visitor.objects.all().__len__() >MAX_USER_LOGIN_LIMIT:
+            result={
+                            "success": 1,     # 0 - fail, 1 - success, 2 - exception
+                            "message": "Success/Fail message.",
+                            "data": {
+                                "meta": {},
+                                "objects": {'user_limit_exceed':True }
+                                    }
+                      }
+
+            return HttpResponse(json.dumps(result), mimetype='application/json')
 
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
@@ -29,68 +39,37 @@ def auth_view(request):
                 session_key_in_visitor_db = request.user.visitor.session_key
 
                 if session_key_in_visitor_db != key_from_cookie:
+                    objects_values=dict(dialog=True)
 
-                    result={
-                                "success": 1,     # 0 - fail, 1 - success, 2 - exception
-                                "message": "Success/Fail message.",
-                                "data": {
-                                    "meta": {},
-                                    "objects": {
-                                         'dialog':True
-                                                }
-                                        }
-                          }
-
-                    return HttpResponse(json.dumps(result), mimetype='application/json')
                 else:
                     #User Log Activity
                     action.send(request.user, verb=u'username : %s loggedin from server name: %s, server port: %s'\
                                      %( username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
+                    objects_values=dict(url='/home/')
 
-                    result={
-                                "success": 1,     # 0 - fail, 1 - success, 2 - exception
-                                "message": "Success/Fail message.",
-                                "data": {
-                                    "meta": {},
-                                    "objects": {
-                                         'url':'/home/',
-                                                }
-                                        }
-                          }
-
-                    return HttpResponse(json.dumps(result), mimetype='application/json')
             else:
                 Visitor.objects.create( user=request.user, session_key=key_from_cookie )
                 #User Log Activity
                 action.send(request.user, verb=u'username : %s loggedin from server name: %s, server port: %s'\
                                 %( username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
-                result={
-                                "success": 1,     # 0 - fail, 1 - success, 2 - exception
-                                "message": "Success/Fail message.",
-                                "data": {
-                                    "meta": {},
-                                    "objects": {
-                                         'url':'/home/',
-                                                }
-                                        }
-                          }
-                return HttpResponse(json.dumps(result), mimetype='application/json')
+                objects_values=dict(url='/home/')
+
         else:
             #User Log Activity
             action.send(User.objects.get(pk=1), verb=u'invalid loggedin using username : %s from server name: %s, '
                          u'server port: %s' %(username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
-            result={
-                                "success": 1,     # 0 - fail, 1 - success, 2 - exception
-                                "message": "Success/Fail message.",
-                                "data": {
-                                    "meta": {},
-                                    "objects": {
-                                         'url':'/login/',
-                                                }
-                                        }
-                          }
+            objects_values=dict(url='/login/')
 
-            return HttpResponse(json.dumps(result), mimetype='application/json')
+        result={
+                            "success": 1,     # 0 - fail, 1 - success, 2 - exception
+                            "message": "Success/Fail message.",
+                            "data": {
+                                "meta": {},
+                                "objects": objects_values
+                                    }
+                      }
+
+        return HttpResponse(json.dumps(result), mimetype='application/json')
 
 def logout(request):
     auth.logout(request)
