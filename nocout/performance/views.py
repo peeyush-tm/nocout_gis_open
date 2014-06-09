bde379748f6c9f0b8d646f6b2a61828c2341cb5f
yogender-tm <yogender@teramatrix.co>
2014-06-09 18:07:28 +0530

    Create templates for performance app & make respective changes in 'view.py' and 'url.py'

--- nocout/performance/views.py
+++ nocout/performance/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views.generic.base import View
+from django.template import RequestContext
 
 # Create your views here.
+
+def get_live_performance(request, page_type = "no_page"):
+
+	# Customer live performance case
+	if(page_type == "customer"):
+		return render_to_response('performance/customer_perf.html',context_instance=RequestContext(request))
+	# Network live performance case
+	elif(page_type == "network"):
+		return render_to_response('performance/network_perf.html',context_instance=RequestContext(request))
+	# Other live performance case
+	else:
+		return render_to_response('performance/other_perf.html',context_instance=RequestContext(request))
+
+def get_performance_dashboard(request):
+
+	return render_to_response('performance/perf_dashboard.html',context_instance=RequestContext(request))
+
+
+def get_sector_dashboard(request):
+
+	return render_to_response('performance/sector_dashboard.html',context_instance=RequestContext(request))
