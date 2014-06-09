67f0229736a5c16ca60ea7a939316c20a41fede4
Peeyush Raj <praj@teramatrix.in>
2014-06-09 14:49:19 +0530

    proper messages to user when logging in

--- nocout/nocout/views.py
+++ nocout/nocout/views.py
@@ -16,62 +16,102 @@ def login(request):
 
 def auth_view(request):
 
-        if Visitor.objects.all().__len__() >MAX_USER_LOGIN_LIMIT:
-            result={
-                            "success": 1,     # 0 - fail, 1 - success, 2 - exception
-                            "message": "Success/Fail message.",
-                            "data": {
-                                "meta": {},
-                                "objects": {'user_limit_exceed':True }
-                                    }
-                      }
+    result = {
+        "success": 2,  # 0 - fail, 1 - success, 2 - exception
+        "message": "Un-handled system exception has occurred. Please check with application administrator.",
+        "data": {
+            "meta": {},
+            "objects": {}
+        }
+    }
 
-            return HttpResponse(json.dumps(result), mimetype='application/json')
+    objects_values=dict(url='/login/')
 
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        user = auth.authenticate(username=username, password=password)
+    if Visitor.objects.all().__len__() >MAX_USER_LOGIN_LIMIT:
+        result = {
+            "success": 1,  # 0 - fail, 1 - success, 2 - exception
+            "message": "Success/Fail message.",
+            "data": {
+                "meta": {},
+                "objects": {'user_limit_exceed': True}
+            }
+        }
 
-        if user is not None and user.is_active:
+        return HttpResponse(json.dumps(result), mimetype='application/json')
 
-            auth.login(request, user)
 
-            key_from_cookie = request.session.session_key
-            if hasattr(request.user, 'visitor'):
-                session_key_in_visitor_db = request.user.visitor.session_key
 
-                if session_key_in_visitor_db != key_from_cookie:
-                    objects_values=dict(dialog=True)
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
 
-                else:
-                    #User Log Activity
-                    action.send(request.user, verb=u'username : %s loggedin from server name: %s, server port: %s'\
-                                     %( username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
-                    objects_values=dict(url='/home/')
+    if user is not None and user.is_active:
+
+        auth.login(request, user)
+
+        key_from_cookie = request.session.session_key
+        if hasattr(request.user, 'visitor'):
+            session_key_in_visitor_db = request.user.visitor.session_key
+
+            if session_key_in_visitor_db != key_from_cookie:
+                objects_values=dict(dialog=True)
 
             else:
-                Visitor.objects.create( user=request.user, session_key=key_from_cookie )
                 #User Log Activity
                 action.send(request.user, verb=u'username : %s loggedin from server name: %s, server port: %s'\
-                                %( username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
+                                 %( username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
                 objects_values=dict(url='/home/')
 
         else:
+            Visitor.objects.create( user=request.user, session_key=key_from_cookie )
             #User Log Activity
-            action.send(User.objects.get(pk=1), verb=u'invalid loggedin using username : %s from server name: %s, '
-                         u'server port: %s' %(username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
-            objects_values=dict(url='/login/')
+            action.send(request.user, verb=u'username : %s loggedin from server name: %s, server port: %s'\
+                            %( username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
+            objects_values=dict(url='/home/')
 
-        result={
-                            "success": 1,     # 0 - fail, 1 - success, 2 - exception
-                            "message": "Success/Fail message.",
-                            "data": {
-                                "meta": {},
-                                "objects": objects_values
-                                    }
-                      }
+        result = {
+            "success": 1,  # 0 - fail, 1 - success, 2 - exception
+            "message": "Logged in successfully.",
+            "data": {
+                "meta": {},
+                "objects": objects_values
+            }
+        }
 
-        return HttpResponse(json.dumps(result), mimetype='application/json')
+    elif not user.is_active:
+        action.send(User.objects.get(pk=1), verb=u'a locked user is loggedin using username : %s from server name: %s, '
+                     u'server port: %s' %(username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
+
+        objects_values=dict(url='/login/')
+
+        result = {
+            "success": 0,  # 0 - fail, 1 - success, 2 - exception
+            "message": "The account has been locked by the application administrator. \
+                        Please contact application administrator to continue.",
+            "data": {
+                "meta": {},
+                "objects": objects_values
+            }
+        }
+
+    else:
+        #User Log Activity
+        action.send(User.objects.get(pk=1), verb=u'invalid loggedin using username : %s from server name: %s, '
+                     u'server port: %s' %(username, request.META.get('SERVER_NAME'), request.META.get('SERVER_PORT')))
+
+        objects_values=dict(url='/login/')
+
+        result = {
+            "success": 0,  # 0 - fail, 1 - success, 2 - exception
+            "message": "The credentials entered can not be verified by the system. \
+                        Please contact application administrator or retry",
+            "data": {
+                "meta": {},
+                "objects": objects_values
+            }
+        }
+
+    return HttpResponse(json.dumps(result), mimetype='application/json')
 
 def logout(request):
     auth.logout(request)
