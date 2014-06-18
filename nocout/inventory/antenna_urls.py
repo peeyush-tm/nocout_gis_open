7c9d9a904b773fe34c0b70810be1bbbfe3889778
Priyesh <priyesh@teramatrix.co>
2014-06-18 15:51:17 +0530

    Creating antenna forms, views & url.

---
+++ nocout/inventory/antenna_urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+from device import views
+
+urlpatterns = patterns('',
+  url(r'^$', views.DeviceTechnologyList.as_view(), name='device_technology_list'),
+  url(r'^(?P<pk>\d+)$', views.DeviceTechnologyDetail.as_view(), name='device_technology_detail'),
+  url(r'^new/$', views.DeviceTechnologyCreate.as_view(), name='device_technology_new'),
+  url(r'^edit/(?P<pk>\d+)$', views.DeviceTechnologyUpdate.as_view(), name='device_technology_edit'),
+  url(r'^delete/(?P<pk>\d+)$', views.DeviceTechnologyDelete.as_view(), name='device_technology_delete'),
+  url(r'^devicetechnologylistingtable/', views.DeviceTechnologyListingTable.as_view(), name='DeviceTechnologyListingTable'),
+)
---
+++ nocout/inventory/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from models import Service, ServiceParameters
+
+
+class ServiceForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ServiceForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            if field.widget.attrs.has_key('class'):
+                field.widget.attrs['class'] += ' form-control'
+            else:
+                field.widget.attrs.update({'class':'form-control'})
+    class Meta:
+        model = Service
+        fields = ('service_name', 'alias', 'parameters', 'service_data_sources', 'command', 'description')
+
+
+class ServiceParametersForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ServiceParametersForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            if field.widget.attrs.has_key('class'):
+                field.widget.attrs['class'] += ' form-control'
+            else:
+                field.widget.attrs.update({'class':'form-control'})
+    class Meta:
+        model = ServiceParameters
+        fields = ('parameter_description', 'max_check_attempts', 'check_interval',
+                  'retry_interval', 'check_period', 'notification_interval',
+                  'notification_period'
+        )
