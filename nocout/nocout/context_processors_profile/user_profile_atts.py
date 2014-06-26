99e6a32d16efef45c70455904a9a2a845ae23bec
nishant <nishant@teramatrix.co>
2014-06-26 16:10:18 +0530

    Password of the user_profile was not getting saved.And myprofile form was not getting updated due to organization field weird behaviuor.

--- nocout/nocout/context_processors_profile/user_profile_atts.py
+++ nocout/nocout/context_processors_profile/user_profile_atts.py
@@ -1,11 +1,8 @@
-from user_profile.models import UserProfile
-from user_group.models import Organization
-
 # context processor for showing user department in template
 
 
 def user_dept_org(request):
     result = {}
-    if request.user.is_authenticated():
-        result['user_organization']=request.user.userprofile.organization.alias
+    if request.user.is_authenticated() and hasattr(request.user, 'userprofile'):
+            result['user_organization']=request.user.userprofile.organization.alias
     return result
