40fcee10f1bd09185e91a8710c5e89ca560668ba
Priyesh <priyesh@teramatrix.co>
2014-07-01 02:13:57 +0530

    Adding models EventNetwork, EventService, EventStatus, EventMachine, EventInventory in performance.

--- nocout/performance/models.py
+++ nocout/performance/models.py
@@ -2,23 +2,120 @@ from django.db import models
 
 
 class PerformanceMetric(models.Model):
-    device_name = models.CharField('Device Name', max_length=100, null=True)
-    service_name = models.CharField('Service Name', max_length=100, null=True)
-    machine_name = models.CharField('Machine Name', max_length=100, null=True)
-    site_name = models.CharField('Site Name', max_length=100, null=True)
-    data_source = models.CharField('Data Source', max_length=100, null=True)
-    current_value = models.CharField('Current Value', max_length=20, default=0)
-    min_value = models.CharField('Min Value', max_length=20, default=0)
-    max_value = models.CharField('Max Value', max_length=20, default=0)
-    avg_value = models.CharField('Avg Value', max_length=20, default=0)
-    warning_threshold = models.CharField('Warning Threshold', max_length=20, default=0)
-    critical_threshold = models.CharField('Critical Threshold', max_length=20, default=0)
-    sys_timestamp = models.IntegerField('SYS Timestamp', null=True)
-    check_timestamp = models.IntegerField('Check Timestamp', null=True)
+    device_name = models.CharField('Device Name', max_length=100, null=True, blank=True)
+    service_name = models.CharField('Service Name', max_length=100, null=True, blank=True)
+    machine_name = models.CharField('Machine Name', max_length=100, null=True, blank=True)
+    site_name = models.CharField('Site Name', max_length=100, null=True, blank=True)
+    data_source = models.CharField('Data Source', max_length=100, null=True, blank=True)
+    severity = models.IntegerField('Severity', null=True, blank=True)
+    current_value = models.CharField('Current Value', max_length=20, null=True, blank=True)
+    min_value = models.CharField('Min Value', max_length=20, null=True, blank=True)
+    max_value = models.CharField('Max Value', max_length=20, null=True, blank=True)
+    avg_value = models.CharField('Avg Value', max_length=20, null=True, blank=True)
+    warning_threshold = models.CharField('Warning Threshold', max_length=20, null=True, blank=True)
+    critical_threshold = models.CharField('Critical Threshold', max_length=20, null=True, blank=True)
+    sys_timestamp = models.IntegerField('SYS Timestamp', null=True, blank=True)
+    check_timestamp = models.IntegerField('Check Timestamp', null=True, blank=True)
 
     def __unicode__(self):
         return self.device_name
 
 
+class EventNetwork(models.Model):
+    device_name = models.CharField('Device Name', max_length=100, null=True, blank=True)
+    service_name = models.CharField('Service Name', max_length=100, null=True, blank=True)
+    machine_name = models.CharField('Machine Name', max_length=100, null=True, blank=True)
+    site_name = models.CharField('Site Name', max_length=100, null=True, blank=True)
+    data_source = models.CharField('Data Source', max_length=100, null=True, blank=True)
+    severity = models.IntegerField('Severity', null=True, blank=True)
+    current_value = models.CharField('Current Value', max_length=20, null=True, blank=True)
+    min_value = models.CharField('Min Value', max_length=20, null=True, blank=True)
+    max_value = models.CharField('Max Value', max_length=20, null=True, blank=True)
+    avg_value = models.CharField('Avg Value', max_length=20, null=True, blank=True)
+    warning_threshold = models.CharField('Warning Threshold', max_length=20, null=True, blank=True)
+    critical_threshold = models.CharField('Critical Threshold', max_length=20, null=True, blank=True)
+    sys_timestamp = models.IntegerField('SYS Timestamp', null=True, blank=True)
+    check_timestamp = models.IntegerField('Check Timestamp', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.device_name
 
 
+class EventService(models.Model):
+    device_name = models.CharField('Device Name', max_length=100, null=True, blank=True)
+    service_name = models.CharField('Service Name', max_length=100, null=True, blank=True)
+    machine_name = models.CharField('Machine Name', max_length=100, null=True, blank=True)
+    site_name = models.CharField('Site Name', max_length=100, null=True, blank=True)
+    data_source = models.CharField('Data Source', max_length=100, null=True, blank=True)
+    severity = models.IntegerField('Severity', null=True, blank=True)
+    current_value = models.CharField('Current Value', max_length=20, null=True, blank=True)
+    min_value = models.CharField('Min Value', max_length=20, null=True, blank=True)
+    max_value = models.CharField('Max Value', max_length=20, null=True, blank=True)
+    avg_value = models.CharField('Avg Value', max_length=20, null=True, blank=True)
+    warning_threshold = models.CharField('Warning Threshold', max_length=20, null=True, blank=True)
+    critical_threshold = models.CharField('Critical Threshold', max_length=20, null=True, blank=True)
+    sys_timestamp = models.IntegerField('SYS Timestamp', null=True, blank=True)
+    check_timestamp = models.IntegerField('Check Timestamp', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.device_name
+
+
+class EventStatus(models.Model):
+    device_name = models.CharField('Device Name', max_length=100, null=True, blank=True)
+    service_name = models.CharField('Service Name', max_length=100, null=True, blank=True)
+    machine_name = models.CharField('Machine Name', max_length=100, null=True, blank=True)
+    site_name = models.CharField('Site Name', max_length=100, null=True, blank=True)
+    data_source = models.CharField('Data Source', max_length=100, null=True, blank=True)
+    severity = models.IntegerField('Severity', null=True, blank=True)
+    current_value = models.CharField('Current Value', max_length=20, null=True, blank=True)
+    min_value = models.CharField('Min Value', max_length=20, null=True, blank=True)
+    max_value = models.CharField('Max Value', max_length=20, null=True, blank=True)
+    avg_value = models.CharField('Avg Value', max_length=20, null=True, blank=True)
+    warning_threshold = models.CharField('Warning Threshold', max_length=20, null=True, blank=True)
+    critical_threshold = models.CharField('Critical Threshold', max_length=20, null=True, blank=True)
+    sys_timestamp = models.IntegerField('SYS Timestamp', null=True, blank=True)
+    check_timestamp = models.IntegerField('Check Timestamp', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.device_name
+
+
+class EventMachine(models.Model):
+    device_name = models.CharField('Device Name', max_length=100, null=True, blank=True)
+    service_name = models.CharField('Service Name', max_length=100, null=True, blank=True)
+    machine_name = models.CharField('Machine Name', max_length=100, null=True, blank=True)
+    site_name = models.CharField('Site Name', max_length=100, null=True, blank=True)
+    data_source = models.CharField('Data Source', max_length=100, null=True, blank=True)
+    severity = models.IntegerField('Severity', null=True, blank=True)
+    current_value = models.CharField('Current Value', max_length=20, null=True, blank=True)
+    min_value = models.CharField('Min Value', max_length=20, null=True, blank=True)
+    max_value = models.CharField('Max Value', max_length=20, null=True, blank=True)
+    avg_value = models.CharField('Avg Value', max_length=20, null=True, blank=True)
+    warning_threshold = models.CharField('Warning Threshold', max_length=20, null=True, blank=True)
+    critical_threshold = models.CharField('Critical Threshold', max_length=20, null=True, blank=True)
+    sys_timestamp = models.IntegerField('SYS Timestamp', null=True, blank=True)
+    check_timestamp = models.IntegerField('Check Timestamp', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.device_name
+
+
+class EventInventory(models.Model):
+    device_name = models.CharField('Device Name', max_length=100, null=True, blank=True)
+    service_name = models.CharField('Service Name', max_length=100, null=True, blank=True)
+    machine_name = models.CharField('Machine Name', max_length=100, null=True, blank=True)
+    site_name = models.CharField('Site Name', max_length=100, null=True, blank=True)
+    data_source = models.CharField('Data Source', max_length=100, null=True, blank=True)
+    severity = models.IntegerField('Severity', null=True, blank=True)
+    current_value = models.CharField('Current Value', max_length=20, null=True, blank=True)
+    min_value = models.CharField('Min Value', max_length=20, null=True, blank=True)
+    max_value = models.CharField('Max Value', max_length=20, null=True, blank=True)
+    avg_value = models.CharField('Avg Value', max_length=20, null=True, blank=True)
+    warning_threshold = models.CharField('Warning Threshold', max_length=20, null=True, blank=True)
+    critical_threshold = models.CharField('Critical Threshold', max_length=20, null=True, blank=True)
+    sys_timestamp = models.IntegerField('SYS Timestamp', null=True, blank=True)
+    check_timestamp = models.IntegerField('Check Timestamp', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.device_name
