f148bc00507daa6b84d186cea74f48b6f0338e83
nishant <nishant@teramatrix.co>
2014-07-07 09:41:11 +0530

    [#74450348] Customer Ajax URLS.

--- nocout/performance/views.py
+++ nocout/performance/views.py
@@ -5,9 +5,10 @@ from django.views.generic.base import View
 from django.template import RequestContext
 
 # Create your views here.
-from device.models import Device, City, State
+from device.models import Device, City, State, DeviceType
 
 from inventory.models import SubStation, Circuit, Sector, BaseStation
+from performance.models import PerformanceService
 
 
 def get_live_performance(request, page_type = "no_page"):
@@ -28,8 +29,8 @@ def get_performance(request, page_type = "no_page", device_id=0):
 					'device_id' : device_id,
 					'get_devices_url' : 'get_substation_devices/',
 					'get_status_url' : 'get_substation_status/'+str(device_id)+'/',
-					'get_services_url' : 'get_services',
-					'get_service_data_url' : 'get_service_data'
+					'get_services_url' : 'get_substation_services'+str(device_id)+'/',
+					'get_service_data_url' : 'get_substation_services_data/'+str(device_id)+'/'
 				}
 
 	return render_to_response('performance/single_device_perf.html',page_data,context_instance=RequestContext(request))
@@ -93,7 +94,6 @@ def get_substation_status(request, device_id):
         }
     }
 
-
     substation= SubStation.objects.get(id=device_id)
     substation_device= Device.objects.get(id= substation.device_id)
     sector= Circuit.objects.get(sub_station= substation.id).sector
@@ -113,14 +113,56 @@ def get_substation_status(request, device_id):
     result['message']='Substation Devices Fetched Successfully.'
     return HttpResponse(json.dumps(result))
 
+def get_substation_services(request, device_id):
+    result={
+        'success' : 0,
+        'message' : 'Substation Devices Not Fetched Successfully.',
+        'data' : {
+            'meta' : {},
+            'objects' : []
+        }
+    }
+
+    substation= SubStation.objects.get(id= device_id)
+    substation_device_type_id= Device.objects.get(id= substation.device_id).device_type
+    substation_device_services_name= DeviceType.objects.get(id= substation_device_type_id).service.values_list('name', 'alias')
+    for substation_device_service_name in substation_device_services_name:
+
+        result['data']['objects'].append({
+            'name':substation_device_service_name[0],
+            'title':substation_device_service_name[1],
+            'url':'service/'+ substation_device_service_name[0] +'/data/'+str(device_id),
+            'active':1
+        })
+
+    result['success']=1
+    result['message']='Substation Devices Services name Fetched Successfully.'
+    return HttpResponse(json.dumps(result))
 
 
 
+class Get_Service_Type_Performance_Data(View):
 
-
-
-
-
-
+    def get(self, request, service_type, device_id):
+        result={
+        'success' : 0,
+        'message' : 'Substation Service Not Fetched Successfully.',
+        'data' : {
+            'meta' : {},
+            'objects' : {}
+            }
+        }
+        substation= SubStation.objects.get(id= int(device_id))
+        substation_device_name= Device.objects.get(id= substation.device_id).device_name
+        performance_data=PerformanceService.objects.filter(service_name=service_type, device_name=substation_device_name)\
+            .values_list('check_timestamp','avg_value')
+        performance_data=map(list, performance_data)
+        result['data']['objects']['type']='line'
+        result['data']['objects']['chart_data']=[
+            {'name': 'Latencay', 'color':'#70AFC4', 'data':performance_data if performance_data else None }
+        ]
+        result['success']=1
+        result['message']='Substation Service Fetched Successfully.'
+        return HttpResponse(json.dumps(result), mimetype="application/json")
 
 
