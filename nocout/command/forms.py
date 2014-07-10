40bdebd39d650b7387a90c5e9900b004af7ef9ff
Priyesh <priyesh@teramatrix.co>
2014-07-10 12:58:11 +0530

    Adding mandatory fields identifier in 'user_profile', 'user_group', 'site_instance', 'service', 'organization', 'machine', 'command'.

--- nocout/command/forms.py
+++ nocout/command/forms.py
@@ -8,8 +8,16 @@ class CommandForm(forms.ModelForm):
         super(CommandForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
             if field.widget.attrs.has_key('class'):
-                field.widget.attrs['class'] += ' form-control'
+                if isinstance(field.widget, forms.widgets.Select):
+                    field.widget.attrs['class'] += ' col-md-12'
+                    field.widget.attrs['class'] += ' select2select'
+                else:
+                    field.widget.attrs['class'] += ' form-control'
             else:
-                field.widget.attrs.update({'class':'form-control'})
+                if isinstance(field.widget, forms.widgets.Select):
+                    field.widget.attrs.update({'class': 'col-md-12 select2select'})
+                else:
+                    field.widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Command
