852cfd7876ae81536dbaf1a44ab6c45973845c8e
Priyesh <priyesh@teramatrix.co>
2014-07-14 18:01:09 +0530

    Adding 'web_service_port', 'username', 'password' in site instance model to construct dynamic url for nocout.py.

--- nocout/site_instance/models.py
+++ nocout/site_instance/models.py
@@ -8,8 +8,10 @@ class SiteInstance(models.Model):
     machine = models.ForeignKey(Machine, null=True, blank=True)
     site_ip = models.IPAddressField('IP Address')
     live_status_tcp_port = models.IntegerField('Live Status TCP Port', null=True, blank=True)
+    web_service_port = models.IntegerField('Web Service Port', null=True, blank=True)
+    username = models.CharField('Username', max_length=100, null=True, blank=True)
+    password = models.CharField('Password', max_length=100, null=True, blank=True)
     description = models.TextField('Description', null=True, blank=True)
 
     def __unicode__(self):
         return self.name
-    
