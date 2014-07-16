f54c8cecb7c111e0a8c1a0be887c3b5ad3bcc196
nishant <nishant@teramatrix.co>
2014-07-16 19:27:58 +0530

    performance graph url changes.

--- nocout/performance/urls.py
+++ nocout/performance/urls.py
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
 	url(r'^get_inventory_device_status/(?P<page_type>\w+)/device/(?P<device_id>\d+)/$', views.Inventory_Device_Status.as_view()),
 	url(r'^get_inventory_service_data_sources/(?P<page_type>\w+)/device/(?P<device_id>\d+)/$',
         views.Inventory_Device_Service_Data_Source.as_view()),
-	url(r'^service_data_source/(?P<service_data_source_type>\w+)/(?P<page_type>\w+)/device/(?P<device_id>\d+)$', Get_Service_Type_Performance_Data.as_view())
+	url(r'^service/(?P<service_name>\w+)/service_data_source/(?P<service_data_source_type>\w+)/(?P<page_type>\w+)/device/(?P<device_id>\d+)$', Get_Service_Type_Performance_Data.as_view())
 )
