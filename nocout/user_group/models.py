55433dca5ee9a89296c30226d90ca5066cd7ab44
nishant <nishant@teramatrix.co>
2014-07-22 18:00:50 +0530

    [#75430444] Adding migration to delete the address field in user_group table.

--- nocout/user_group/models.py
+++ nocout/user_group/models.py
@@ -11,7 +11,6 @@ class UserGroup(MPTTModel, models.Model):
     users = models.ManyToManyField(UserProfile)
     parent = TreeForeignKey('self', null=True, blank=True, related_name='usergroup_children')
     organization = models.ForeignKey(Organization)
-    address = models.CharField('Address', max_length=100, null=True, blank=True)
     is_deleted = models.IntegerField('Is Deleted', max_length=1, default=0)
 
     def __unicode__(self):
