31506f8dbebcc79a0d13223d42829c188e2b4dc2
nishant <nishant@teramatrix.co>
2014-07-23 19:53:44 +0530

    Removing IP field from site instance and adding latitude and longitude in the Substation.

--- nocout/site_instance/models.py
+++ nocout/site_instance/models.py
@@ -6,7 +6,6 @@ class SiteInstance(models.Model):
     name = models.CharField('Name', max_length=200, unique=True)
     alias = models.CharField('Alias', max_length=255)
     machine = models.ForeignKey(Machine, null=True, blank=True)
-    site_ip = models.IPAddressField('IP Address')
     live_status_tcp_port = models.IntegerField('Live Status TCP Port', null=True, blank=True)
     web_service_port = models.IntegerField('Web Service Port', default=80)
     username = models.CharField('Username', max_length=100, null=True, blank=True)
