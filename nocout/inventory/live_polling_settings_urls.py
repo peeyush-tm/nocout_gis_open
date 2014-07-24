99b9a94ff46e7c097210dbd761d61d2f816808fc
Priyesh <priyesh@teramatrix.co>
2014-07-24 18:46:28 +0530

    Adding 'live polling settings' urls.

--- nocout/inventory/live_polling_settings_urls.py
+++ nocout/inventory/live_polling_settings_urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 from inventory import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.IconSettingsList.as_view(), name='icon_settings_list'),
-  url(r'^(?P<pk>\d+)$', views.IconSettingsDetail.as_view(), name='icon_settings_detail'),
-  url(r'^new/$', views.IconSettingsCreate.as_view(), name='icon_settings_new'),
-  url(r'^edit/(?P<pk>\d+)$', views.IconSettingsUpdate.as_view(), name='icon_settings_edit'),
-  url(r'^delete/(?P<pk>\d+)$', views.IconSettingsDelete.as_view(), name='icon_settings_delete'),
-  url(r'^IconSettingslistingtable/', views.IconSettingsListingTable.as_view(), name='IconSettingsListingTable'),
+  url(r'^$', views.LivePollingSettingsList.as_view(), name='live_polling_settings_list'),
+  url(r'^(?P<pk>\d+)$', views.LivePollingSettingsDetail.as_view(), name='live_polling_settings_detail'),
+  url(r'^new/$', views.LivePollingSettingsCreate.as_view(), name='live_polling_settings_new'),
+  url(r'^edit/(?P<pk>\d+)$', views.LivePollingSettingsUpdate.as_view(), name='live_polling_settings_edit'),
+  url(r'^delete/(?P<pk>\d+)$', views.LivePollingSettingsDelete.as_view(), name='live_polling_settings_delete'),
+  url(r'^LivePollingSettingslistingtable/', views.LivePollingSettingsListingTable.as_view(), name='LivePollingSettingsListingTable'),
 )
