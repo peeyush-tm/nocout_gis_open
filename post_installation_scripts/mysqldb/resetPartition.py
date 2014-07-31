6fd1316df54b822d8d93630497e1cedb143e4962
Peeyush <praj@teramatrix.in>
2014-07-31 15:49:35 +0530

    partitioning weekly basis. TODO have partition monthly basis and subpartition weekly basis. LOOK for subpartitioning

--- post_installation_scripts/mysqldb/resetPartition.py
+++ post_installation_scripts/mysqldb/resetPartition.py
@@ -2,12 +2,7 @@ import re
 import datetime
 import calendar
 
-count = 0
 count_year = int(raw_input("how many years:>"))  # license validity
-now = datetime.datetime.now()
-
-cur_yer = now.year
-cur_mon = now.month
 
 partitionTableList = [
     "performance_eventinventory",
@@ -28,13 +23,20 @@ partitionTableList = [
     "performance_status"
 ]
 
-if cur_mon != 1:
+now = datetime.datetime.now()
+cur_yer = now.year
+cur_mon = now.month
+
+if cur_mon > 1:
     count_year += 1
+
+years_list = range(cur_yer, cur_yer + count_year + 1)
 
 table_column = "sys_timestamp"
 
 for ptable in partitionTableList:
-    count += 1
+
+
     fwrite = open("resetPartition.sql", 'a')
 
     # ##
@@ -50,60 +52,35 @@ for ptable in partitionTableList:
         "PARTITION BY RANGE(sys_timestamp)" + "\n")
     fwrite.write("(" + "\n")
 
-    i = cur_yer
-
-    # while j < count_year:
-    #     c = calendar.Calendar()
-    #     c.setfirstweekday(calendar.SUNDAY)
-    #     weeks = c.yeardatescalendar(i)
-    #     count = 0
-    #     temp = {}
-    #     writeThis = []
-    #     for week in weeks:
-    #         for w in week:
-    #             for ws in w:
-    #                 if str(ws[-1]) not in temp:
-    #                     count += 1
-    #                     temp[str(ws[-1])] = ""
-    #                     temp[str(ws[-1])] = str(ws[-1]) + " " + "00:00:00"
-    #                     writeThis.append("PARTITION p" + str(count * (i + count - 1)) +
-    #                                      " VALUES LESS THAN (UNIX_TIMESTAMP('"+temp[str(ws[-1])]+"'))" + "\n")
-    #     fwrite.write(",".join(writeThis))
-    #     i += 1
-    #     j += 1
-
     temp_partition = {}
-    for yearstocome in range(count_year):
-        if i not in temp_partition:
-            if i == cur_yer:
-                temp_partition[i] = range(cur_mon, 13)
-            elif i == cur_yer + count_year - 1:
-                temp_partition[i] = range(1, cur_mon+1)
+
+    for year in years_list:
+        if year not in temp_partition:
+            if year == cur_yer:
+                temp_partition[year] = range(cur_mon, 13)
+            elif year == (cur_yer + count_year):
+                temp_partition[year] = range(1, cur_mon+1)
             else:
-                temp_partition[i] = range(1, 13)
-        else:
-            pass
+                temp_partition[year] = range(1, 13)
 
-        i += 1
-
-    i = cur_yer
+    print temp_partition
 
     c = calendar.Calendar()
     c.setfirstweekday(calendar.SUNDAY)
     writeThis = []
     temp = {}
-    for year in temp_partition:
+    for year in years_list:
         for month in temp_partition[year]:
-            weeks = c.monthdatescalendar(i, month)
             count = 0
+            weeks = c.monthdatescalendar(year, month)
             for week in weeks:
                 if str(week[-1]) not in temp:
                     count += 1
                     temp[str(week[-1])] = ""
                     temp[str(week[-1])] = str(week[-1]) + " " + "00:00:00"
-                    writeThis.append("PARTITION p" + str(i) + str(month) + str(count) +
+                    writeThis.append("PARTITION p" + str(year) + str(month) + str(count) +
                                      " VALUES LESS THAN (UNIX_TIMESTAMP('"+temp[str(week[-1])]+"'))" + "\n")
-        i += 1
+
     fwrite.write(",".join(writeThis))
 
     fwrite.write("); \n")
