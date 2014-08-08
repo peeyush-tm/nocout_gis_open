0709c95619e552ee6b6584ae6ae1ffa38a4b99ff
Priyesh <priyesh@teramatrix.co>
2014-08-08 17:58:16 +0530

    Apply specific special characters(., _, -) validation for name in 'machine' forms.

--- nocout/machine/forms.py
+++ nocout/machine/forms.py
@@ -1,5 +1,9 @@
+import re
 from django import forms
 from models import Machine
+from django.forms.util import ErrorList
+import logging
+logger = logging.getLogger(__name__)
 
 
 #**************************************** Machine *********************************************
@@ -23,8 +27,25 @@ class MachineForm(forms.ModelForm):
                     field.widget.attrs.update({'class': 'col-md-12 select2select'})
                 else:
                     field.widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         """
         Meta Information.
         """
         model = Machine
+
+    def clean(self):
+        """
+        Validations for machine form
+        """
+        name = self.cleaned_data.get('name')
+
+        # check that name must be alphanumeric & can only contains .(dot), -(hyphen), _(underscore).
+        try:
+            if not re.match(r'^[A-Za-z0-9\._-]+$', name):
+                self._errors['name'] = ErrorList(
+                    [u"Name must be alphanumeric & can only contains .(dot), -(hyphen) and _(underscore)."])
+        except Exception as e:
+            logger.info(e.message)
+        return self.cleaned_data
+
