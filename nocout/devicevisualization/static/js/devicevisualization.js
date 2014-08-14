5d9e5a3a9654070192e457c4bd08ac25f5624d86
yogender-tm <yogender@teramatrix.co>
2014-08-14 19:02:06 +0530

    Previous spinner code replace with custom library function

--- nocout/devicevisualization/static/js/devicevisualization.js
+++ nocout/devicevisualization/static/js/devicevisualization.js
@@ -115,14 +115,16 @@ $("#resetFilters").click(function(e) {
  */
 function showAdvFilters() {
 
-    show_spinner();
+    /*Show the spinner*/
+    showSpinner();
     advSearch.getFilterInfo("filterInfoModal","Advance Filters","advFilterBtn",getFilterApi,setFilterApi);
 }
     
 /*If 'Filter' button of advance filter is clicked*/
 $("#setAdvFilterBtn").click(function(e) {
 
-    show_spinner();
+    /*Show spinner*/
+    showSpinner();
 
     /*Reset advance filter status flag*/
     hasAdvFilter = 1;
@@ -341,43 +343,4 @@ function get_page_status() {
     }
 
     $("#gis_status_txt").html(status_txt);
-}
-
-/**
- * This function show spinner when any ajax call is made
- */
-function show_spinner() {
-
-    /*Spinner configuration object*/
-    var spinner_options = {
-        lines: 17, // The number of lines to draw
-        length: 33, // The length of each line
-        width: 3, // The line thickness
-        radius: 19, // The radius of the inner circle
-        corners: 1, // Corner roundness (0..1)
-        rotate: 0, // The rotation offset
-        direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#000', // #rgb or #rrggbb or array of colors
-        speed: 0.7, // Rounds per second
-        trail: 60, // Afterglow percentage
-        shadow: true, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: '50%', // Top position relative to parent
-        left: '50%' // Left position relative to parent
-    },
-    /*Spinner DOM Element*/
-    dom_target = document.getElementById('ajax_spinner')
-
-    if($("#ajax_spinner").hasClass("hide")) {
-        /*Show ajax_spinner div*/
-        $("#ajax_spinner").removeClass("hide")
-        /*Initialize spinner object*/
-        var spinner = new Spinner(spinner_options).spin(dom_target);
-        /*If ajax_backdrop div not exist then appent it to body */
-        if($("#ajax_backdrop").length == 0) {
-            $("body").append('<div class="modal-backdrop fade in" id="ajax_backdrop" style="background:#FFFFFF;"></div>');
-        }
-    }
 }
