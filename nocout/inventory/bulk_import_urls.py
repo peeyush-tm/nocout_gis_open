02656dfd660c4ce50ac4df1c1480870e2e8cb13f
Priyesh <priyesh@teramatrix.co>
2014-08-14 16:21:40 +0530

    Adding url file for bulk upload.

--- nocout/inventory/bulk_import_urls.py
+++ nocout/inventory/bulk_import_urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import patterns, url
 from inventory import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.CircuitList.as_view(), name='circuits_list'),
-  url(r'^(?P<pk>\d+)$', views.CircuitDetail.as_view(), name='circuit_detail'),
-  url(r'^new/$', views.CircuitCreate.as_view(), name='circuit_new'),
-  url(r'^edit/(?P<pk>\d+)$', views.CircuitUpdate.as_view(), name='circuit_edit'),
-  url(r'^delete/(?P<pk>\d+)$', views.CircuitDelete.as_view(), name='circuit_delete'),
-  url(r'^Circuitlistingtable/', views.CircuitListingTable.as_view(), name='CircuitListingTable'),
+  url(r'^$', views.GISInventoryBulkImport.as_view(), name='gis_inventory_bulk_import'),
+  url(r'^gis_inventory/$', views.GISInventoryBulkImport.as_view(), name='gis_inventory_bulk_import'),
+  #url(r'^gis_inventory_bulk_processing/$', views.GISInventoryBulkProcessing.as_view(), name='gis_inventory_bulk_processing')
 )
