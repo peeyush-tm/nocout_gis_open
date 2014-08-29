7ba579d4764995991a016a7111f43890daacca32
Peeyush <praj@teramatrix.in>
2014-08-29 17:48:52 +0530

    frequency as MHz

--- nocout/device/forms.py
+++ nocout/device/forms.py
@@ -727,9 +727,9 @@ class DeviceFrequencyForm(forms.ModelForm):
 
         # check that name must be alphanumeric & can only contains .(dot), -(hyphen), _(underscore).
         try:
-            if not re.match(r'^[A-Za-z0-9\._-]+$', name):
+            if not re.match(r'^[A-Za-z0-9\.]+$', name):
                 self._errors['value'] = ErrorList(
-                    [u"Value must be alphanumeric & can only contains .(dot), -(hyphen) and _(underscore)."])
+                    [u"Value must be alphanumeric & can only contains .(dot)"])
         except Exception as e:
             logger.info(e.message)
         return self.cleaned_data
