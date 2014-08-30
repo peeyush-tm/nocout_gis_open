ee88f7b0f945500a5cf3bbef85cfb8c4b1060cc6
nishant <nishant@teramatrix.co>
2014-08-30 06:58:56 +0530

    Improvement: Adavance Filter not working properly as per technology, per vendor, per sector_configured_on,per circuit_id . STATUS:(Resolved).

--- nocout/devicevisualization/static/js/advanceSearchLib.js
+++ nocout/devicevisualization/static/js/advanceSearchLib.js
@@ -12,7 +12,8 @@ var advSearch_self = "",
     appliedAdvFilter_Active = [],
     searchParameters = "",
     lastSelectedValues = [];
-
+    searchquery_data=[];
+    result_plot_devices=[];
 /**
  * This class is used to create th filter form by calling the get_filter API & the call the set_filter API with the selected filters
  * @class advanceSearchLib
@@ -437,7 +438,72 @@ function advanceSearchClass() {
 
     };
 
-
+//    this.get_device_ids_by_choice_type= function(choice, type)
+//                        {
+//                            choice_type_device_ids_list=[];
+//                            var searchquery_data=[];
+////                                main_devices_data_gmaps;
+//                            for (var q=0;q<searchquery_data.length; q++)
+//                                {
+//                                    /*If the baststation object sector length is Zero then remove it from the searchquery_data.*/
+//                                    if(searchquery_data.data.param.sector.length ==0 )
+//                                        {
+//                                           searchquery_data.shift();
+//                                        }
+//                                    else
+//                                    {
+//                                        for (var m =0; m<searchquery_data[q].data.param.sector.length; m++)
+//                                        {
+//                                            for (var n=0; n<choice.length; n++)
+//                                            {
+//                                                if(type=='technology')
+//                                                {
+//                                                    /*Removing the sectors which does not match the technology choice in searchquery_data*/
+//                                                    if(choice[n] != searchquery_data[q].data.param.sector[m].technology)
+//                                                        {
+//                                                           searchquery_data.data.param.sector[m].pop()
+//                                                        }
+//                                                }
+//                                                else if (type=='vendor')
+//                                                {
+//                                                    /*Removing the sectors which does not match the vendor choice in searchquery_data*/
+//                                                    if(choice[n] != searchquery_data[q].data.param.sector[m].vendor)
+//                                                        {
+//                                                           searchquery_data.data.param.sector[m].pop()
+//                                                        }
+//                                                }
+//                                                else if (type=='sector_configured_on')
+//                                                {
+//                                                    /*Removing the sectors which does not match the sector_configured_on choice in searchquery_data*/
+//                                                    if(choice[n] != searchquery_data[q].data.param.sector[m].sector_configured_on)
+//                                                        {
+//                                                           searchquery_data.data.param.sector[m].pop()
+//                                                        }
+//                                                }
+//                                                else if (type=='circuit_ids')
+//                                                {
+//                                                    /*Removing the sectors which does not match the circuit_ids choice in searchquery_data*/
+//                                                    if(choice[n] != searchquery_data[q].data.param.sector[m].circuit_ids)
+//                                                        {
+//                                                           searchquery_data.data.param.sector[m].pop()
+//                                                        }
+//                                                }
+//                                            }
+//                                            /*If basestation sector length become Zero after removing the
+//                                            non choice sectors then delete the basestation object in the searchquery_data*/
+//                                            if(searchquery_data.data.param.sector.length ==0 )
+//                                            {
+//                                               searchquery_data.shift();
+//                                            }
+//                                        }
+//                                   }
+//                                }
+//                                for (var n=0;n<searchquery_data.data.length; n++)
+//                                {
+//                                   choice_type_device_ids_list.push(searchquery_data.id)
+//                                }
+//                        return choice_type_device_ids_list
+//                        };
 	/**
 	 * This method generates get parameter for setfilter API & call setFilter function
 	 * @method callSetFilter
@@ -452,26 +518,23 @@ function advanceSearchClass() {
 			var selectId = filtersInfoArray[j].key;
 			/*Check Element Type*/
 			if(elementType == "multiselect") {
-
-				var val = $("#filter_"+selectId).select2("val");
-				if(val.length > 0){
-                    for(var k=0; k<val.length; k++)
-                    {
-                        if( val[k].indexOf(',')>=0){
-                          appliedAdvFilter=appliedAdvFilter.concat(val[k].split(','))
-                        }
-                        else{
-                            appliedAdvFilter.push(val[k])
-                         }
-                    }
-
-                appliedAdvFilter_Active.push({'field':selectId, 'value':val});
-                }
-//                appliedAdvFilter= appliedAdvFilter.concat(val);
-            }
-            console.log(appliedAdvFilter)
-
-
+				            var val = $("#filter_"+selectId).select2("val");
+                            if(val.length > 0)
+                            {
+                                for(var k=0; k<val.length; k++)
+                                {
+                                    if( val[k].indexOf(',')>=0)
+                                    {
+                                      appliedAdvFilter.push({'selectId':selectId, 'ids': val[k].split(',')})
+                                    }
+                                    else
+                                    {
+                                      appliedAdvFilter.push({'selectId':selectId, 'ids': val[k] })
+                                    }
+                                }
+                            }
+                    appliedAdvFilter_Active.push({ 'field':selectId, 'value':val });
+		            }}
 //			else if(elementType == "select") {
 //
 //				var selectedVal = $("#filter_"+selectId).select2("val");
@@ -530,17 +593,51 @@ function advanceSearchClass() {
 //
 //				appliedAdvFilter.push(resultantObject);
 //			}
-		}
+        var direct_search_options= ['name','city', 'state','latitude','longitude'];
+        var direct_search_bs_ids= [];
+        var depth_search_bs_ids= [];
+        for(var a=0; a<appliedAdvFilter.length; a++)
+        {
+            if($.inArray(appliedAdvFilter[a].selectId, direct_search_options) >=0)
+            {
+                direct_search_bs_ids=direct_search_bs_ids.concat(appliedAdvFilter[a].ids)
+            }
+            else
+            {
+                depth_search_bs_ids=depth_search_bs_ids.concat(appliedAdvFilter[a].ids)
 
-        appliedAdvFilter= advSearch_self.unique_list(appliedAdvFilter);
+            }
+        }
+        direct_search_bs_ids= advSearch_self.unique_list(direct_search_bs_ids);
+
+        /*Intersection of BS ids.The advance filters are in or condition so, depth search is only required for the
+        unique ids so there by calculating interection between two arrays*/
+        if (depth_search_bs_ids.length>0){
+            depth_search_bs_ids= (direct_search_bs_ids).concat(depth_search_bs_ids);
+            depth_search_bs_ids= advSearch_self.unique_list(depth_search_bs_ids);
+        }
+
+//        appliedAdvFilter= advSearch_self.unique_list(appliedAdvFilter);
 		/*Stringify the object array to pass it in the query parameters for in set filter API*/
-		searchParameters = JSON.stringify(appliedAdvFilter);
 
-		/*call the setFilters function with the searchparamerts & setFilters API url*/
-		advSearch_self.setFilters(searchParameters);
+		/*call the setFilters function with the search parameters & setFilters API url*/
+        if (direct_search_bs_ids.length>0){
+            console.log('direct_search_bs_ids');
+		    direct_search_bs_ids = JSON.stringify(direct_search_bs_ids);
+		    advSearch_self.setFilters(direct_search_bs_ids, 'direct' );
+        }
+        if (depth_search_bs_ids.length>0){
+            console.log('depth_search_bs_ids');
+		    depth_search_bs_ids = JSON.stringify(depth_search_bs_ids);
+		    advSearch_self.setFilters(depth_search_bs_ids, 'depth' );
+        }
+        /*Call the tp plot result of the devices to plot*/
+        advSearch_self.result_plotting();
 		/*Call the reset function*/
 		advSearch_self.resetVariables();
 
+         /*Hide the spinner*/
+        hideSpinner();
 		$("#advFilterFormContainer").html("");
 
 		if(!($("#advFilterContainerBlock").hasClass("hide"))) {
@@ -556,7 +653,8 @@ function advanceSearchClass() {
 		/*Enable the refresh button*/
 		$("#resetFilters").button("loading");
 
-	};
+	}
+
 
     this.unique_list= function(list){
                                   var result_list = [];
@@ -573,26 +671,60 @@ function advanceSearchClass() {
 	 * @param setFilterApi "String" It is the URL of the set filter API
 	 */
 
-	this.setFilters = function(searchString) {
-
+	this.setFilters = function(searchString, search_type) {
+        console.log(searchString);
 		/*Show Remove Filters button*/
 		if($("#removeFilterBtn").hasClass("hide")) {
 			$("#removeFilterBtn").removeClass("hide");
 		}
-        searchString=JSON.parse(searchString)
+        searchString=JSON.parse(searchString);
 
-        var result_plot_device=[];
-        loop1:
-            for(var i= 0;searchString.length>i;i++){
-        loop2:
-                for(var j= 0;main_devices_data_gmaps.length>j;j++){
+        if (search_type=='direct'){
 
-                    if (searchString[i]== main_devices_data_gmaps[j].id){
-                    result_plot_device.push(main_devices_data_gmaps[j])
-                    break loop2
+            loop1:
+                for(var i= 0;searchString.length>i;i++){
+            loop2:
+                    for(var j= 0;main_devices_data_gmaps.length>j;j++){
+
+                        if (searchString[i]== main_devices_data_gmaps[j].id){
+                        result_plot_devices.push(main_devices_data_gmaps[j])
+                        break loop2
+                        }
                     }
                 }
+            console.log(result_plot_devices)
+        }else if(search_type='depth')
+        {
+            var technology_choice=''
+            $.each($('#s2id_filter_technology').select2('data'), function( index, value ){ technology_choice+=value.text })
+            var vendor_choice= ''
+            $.each($('#s2id_filter_vendor').select2('data'), function( index, value ){ vendor_choice+=value.text })
+            var sector_configured_on_choice= ''
+            $.each($('#s2id_filter_sector_configured_on').select2('data'), function( index, value ){ sector_configured_on_choice+=value.text })
+            var circuit_ids_choice= ''
+            $.each($('#s2id_filter_circuit_ids').select2('data'), function( index, value ){ circuit_ids_choice+=value.text })
+
+            for (var i=0; i<main_devices_data_gmaps.length; i++)
+            {
+                var result_plot_data= $.extend( true, {}, main_devices_data_gmaps[i] );
+                result_plot_data.data.param.sector=[];
+                for(var j=0; j<main_devices_data_gmaps[i].data.param.sector.length;j++)
+                {
+
+                    if( technology_choice.indexOf(main_devices_data_gmaps[i].data.param.sector[j].technology) >=0
+                      || vendor_choice.indexOf(main_devices_data_gmaps[i].data.param.sector[j].vendor) >=0
+                      || sector_configured_on_choice.indexOf(main_devices_data_gmaps[i].data.param.sector[j].sector_configured_on) >=0
+                      || circuit_ids_choice.indexOf(main_devices_data_gmaps[i].data.param.sector[j].circuit_id) >=0)
+                    {
+                        result_plot_data.data.param.sector= result_plot_data.data.param.sector.concat(main_devices_data_gmaps[i].data.param.sector[j])
+                    }
+                }
+                if(result_plot_data.data.param.sector.length !=0)
+                {
+                    result_plot_devices.push(result_plot_data)
+                }
             }
+        }
 
         /*Reset The basic filters dropdown*/
         $("#technology").val($("#technology option:first").val());
@@ -624,13 +756,13 @@ function advanceSearchClass() {
 //        } else {
 
             /*Create a instance of networkMapClass*/
-            gmapInstance = new devicePlottingClass_gmap();
+//            gmapInstance = new devicePlottingClass_gmap();
 
             /*Reset markers, polyline & filters*/
-            gmapInstance.clearGmapElements();
+//            gmapInstance.clearGmapElements();
 
             /*Reset Global Variables & Filters*/
-            gmapInstance.resetVariables_gmap();
+//            gmapInstance.resetVariables_gmap();
 
             /*If cluster icon exist then save it to global variable else make the global variable blank*/
 //            if(result.data.objects.data.unspiderfy_icon == undefined) {
@@ -640,7 +772,12 @@ function advanceSearchClass() {
 //            }
 
             /*Call the make network to create the BS-SS network on the google map*/
-            gmapInstance.plotDevices_gmap(result_plot_device,"base_station");
+//            for( var a=0; a<result_plot_devices.length; a++){
+
+//            gmapInstance.plotDevices_gmap(result_plot_devices,"base_station");
+            //Intilaizing value to Null
+//            result_plot_devices=[];
+//            }
             /************** GOOGLE EARTH CODE*********************/
             /*Create a instance of googleEarthClass*/
             // earthInstance = new googleEarthClass();
@@ -675,8 +812,7 @@ function advanceSearchClass() {
 //        $("#removeFilterBtn").click();
 //    }
 
-    /*Hide the spinner*/
-    hideSpinner();
+
     };
 //			},
 //			error : function(err) {
@@ -708,6 +844,35 @@ function advanceSearchClass() {
 	 * @class advanceSearchLib
 	 * @method removeFilters
 	 */
+
+    this.result_plotting= function(){
+                    /*Create a instance of networkMapClass*/
+            gmapInstance = new devicePlottingClass_gmap();
+
+            /*Reset markers, polyline & filters*/
+            gmapInstance.clearGmapElements();
+
+            /*Reset Global Variables & Filters*/
+            gmapInstance.resetVariables_gmap();
+
+            /*If cluster icon exist then save it to global variable else make the global variable blank*/
+//            if(result.data.objects.data.unspiderfy_icon == undefined) {
+//                clusterIcon = "";
+//            } else {
+//                clusterIcon = window.location.origin+"/"+result.data.objects.data.unspiderfy_icon;
+//            }
+
+            /*Call the make network to create the BS-SS network on the google map*/
+//            for( var a=0; a<result_plot_devices.length; a++){
+
+            gmapInstance.plotDevices_gmap(result_plot_devices,"base_station");
+            //Intilaizing value to Null
+            result_plot_devices=[];
+
+
+    }
+
+
 	this.removeFilters = function() {
 
 		/*Reset filter data array*/
