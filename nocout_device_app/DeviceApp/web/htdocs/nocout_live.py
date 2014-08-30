6bc145d8c82d0653f6aa102ce5f97aa827e07341
pardeep-tm <pardeep.singh@teramatrix.co>
2014-08-31 02:27:13 +0530

    bulk live polling

--- nocout_device_app/DeviceApp/web/htdocs/nocout_live.py
+++ nocout_device_app/DeviceApp/web/htdocs/nocout_live.py
@@ -8,10 +8,17 @@ Script to handle on-demand live polling for a particular service data source
 from wato import *
 from pprint import pformat
 import subprocess
+#import time
+import datetime
 import re
 import os
+import signal
 from ast import literal_eval
 import logging
+
+
+class Alarm(Exception):
+	pass
 
 
 def nocout_log():
@@ -32,7 +39,9 @@ def nocout_log():
     os.system('mkdir -p /tmp/nocout_da')
     os.system('chmod 777 /tmp/nocout_da')
     os.system('mkdir -p /tmp/nocout_da/%s' % defaults.omd_site)
+    #os.system('mkdir -p /tmp/nocout_da/pardeep_slave_1')
     fd = os.open('/tmp/nocout_da/%s/nocout_live.log' % defaults.omd_site, os.O_RDWR | os.O_CREAT)
+    #fd = os.open('/tmp/nocout_da/pardeep_slave_1/nocout_live.log', os.O_RDWR | os.O_CREAT)
     if not len(logger.handlers):
         logger.setLevel(logging.DEBUG)
         handler=logging.FileHandler('/tmp/nocout_da/%s/nocout_live.log' % defaults.omd_site)
@@ -99,11 +108,11 @@ def poll_device():
     return current_values
 
 
-def get_current_value(device, service=None, data_source_list=None):
+def get_current_value_old(current_values, device=None, service=None, data_source_list=None):
     response = []
     # Teramatrix poller on which this device is being monitored
     site_name = get_site_name()
-    cmd = '/opt/omd/sites/%s/bin/cmk -nvp %s' % (site_name, device)
+    cmd = '/opt/omd/sites/%s/bin/cmk -nvp --checks=%s %s' % (site_name, service, device)
     # Fork a subprocess
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     check_output, error = p.communicate()
@@ -127,11 +136,63 @@ def get_current_value(device, service=None, data_source_list=None):
 		# Parse the output to get current value for that data source
                 desired_ds = filter(lambda x: ds in x.split('=')[0], ds_values)
                 logger.debug('desired_ds : %s' % desired_ds)
-                response.extend(map(lambda x: x.split('=')[1].split(';')[0], desired_ds))
-            logger.info('response : %s' % response)
+                current_values.extend(map(lambda x: x.split('=')[1].split(';')[0], desired_ds))
+            logger.info('current_values : %s' % current_values)
             logger.info('[Polling Iteration End]')
 
-    return response
+    return current_values
+
+
+def get_current_value(q,device=None, service_list=None, data_source_list=None):
+     #response = []
+     # Teramatrix poller on which this device is being monitored
+     site_name = get_site_name()
+     logger.debug('service_list: ' + pformat(service_list))
+
+     # Pass our custom alarm handler function to signal
+     #signal.signal(signal.SIGALRM, alarm_handler)
+     # Set timeout to 1sec (excepts floats only)
+     #signal.alarm(0.5)
+     for service in service_list:
+	     # Getting result from compiled checks result
+             cmd = '/opt/omd/sites/%s/bin/cmk -nvp --cache --checks=%s %s' % (site_name, service, device)
+             # Fork a subprocess
+             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+	     check_output, error = p.communicate()
+             #logger.debug('Thread started at: ' + pformat(datetime.datetime.now()))
+	     logger.debug('check_out : ' + pformat(check_output))
+             if check_output:
+                 reg_exp1 = re.compile(r'(?<=\()[^)]*(?=\)$)', re.MULTILINE)
+                 # Parse perfdata for all services running on that device
+                 ds_current_states = re.findall(reg_exp1, check_output)
+                 logger.info('ds_current_states : %s' % ds_current_states)
+ 
+                 # Placing all the ds values into one single list
+                 if ds_current_states:
+                         ds_values = ds_current_states[0].split(' ')
+                         logger.info('ds_values : %s' % ds_values)
+                         for ds in data_source_list:
+                                 # Parse the output to get current value for that data source
+                                 desired_ds = filter(lambda x: ds in x.split('=')[0], ds_values)
+                                 logger.debug('desired_ds : %s' % desired_ds)
+                                 data_values = (map(lambda x: x.split('=')[1].split(';')[0], desired_ds))
+				 #q.task_done()
+				 data_dict = {device: data_values}
+				 q.put(data_dict)
+				 #response.append(data_dict)
+				 logger.debug('current_values: ' + pformat(q.qsize()))
+		 else:
+			 data_dict = {device: []}
+			 q.put(data_dict)
+             if error:
+		     # Log the process error code
+		     logging.debug('Process exits with error code: ' + pformat(error))
+     return data_dict
+
+
+def alarm_handler(signum, frame):
+        logger.debug('For loop -----------')
+	raise Alarm
 
 
 def get_site_name(site=None):
