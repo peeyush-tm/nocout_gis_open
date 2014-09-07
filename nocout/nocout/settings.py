3029b6ab9b4aa1b6f31de8ae21df20e57195b9f8
Peeyush Raj <praj@teramatrix.in>
2014-09-07 23:32:38 +0530

    please do not change the settings for media folder and static flders

--- nocout/nocout/settings.py
+++ nocout/nocout/settings.py
@@ -68,7 +68,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
