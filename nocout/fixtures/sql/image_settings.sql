0e6d0f78cb3ce58de1d39c7113584a9fe77bb505
Priyesh <priyesh@teramatrix.co>
2014-09-09 18:44:55 +0530

    Saved SQL files for thematic settings, icon settings and threshold configuration.

---
+++ nocout/fixtures/sql/image_settings.sql
@@ -0,0 +1,26 @@
+/*
+-- Query: SELECT * FROM nocout_dev.inventory_iconsettings
+LIMIT 0, 1000
+
+-- Date: 2014-09-09 18:02
+*/
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (28,'tower1','Tower 1','uploaded/icons/2014-09-09/2014-09-09-13-26-57_mobilephonetower1.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (29,'tower2','Tower 2','uploaded/icons/2014-09-09/2014-09-09-13-28-33_mobilephonetower2.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (30,'tower3','Tower 3','uploaded/icons/2014-09-09/2014-09-09-13-28-50_mobilephonetower3.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (31,'tower4','Tower 4','uploaded/icons/2014-09-09/2014-09-09-13-29-06_mobilephonetower4.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (32,'tower5','Tower 5','uploaded/icons/2014-09-09/2014-09-09-13-29-20_mobilephonetower5.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (33,'tower6','Tower 6','uploaded/icons/2014-09-09/2014-09-09-13-29-36_mobilephonetower6.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (34,'tower7','Tower 7','uploaded/icons/2014-09-09/2014-09-09-13-29-58_mobilephonetower7.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (35,'tower8','Tower 8','uploaded/icons/2014-09-09/2014-09-09-13-30-18_mobilephonetower8.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (36,'tower9','Tower 9','uploaded/icons/2014-09-09/2014-09-09-14-47-11_mobilephonetower9.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (37,'tower10','Tower 10','uploaded/icons/2014-09-09/2014-09-09-13-31-14_mobilephonetower10.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (38,'wifi1','Wifi 1','uploaded/icons/2014-09-09/2014-09-09-13-31-48_wifi1.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (39,'wifi2','Wifi 2','uploaded/icons/2014-09-09/2014-09-09-13-32-06_wifi2.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (40,'wifi3','Wifi 3','uploaded/icons/2014-09-09/2014-09-09-13-32-30_wifi3.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (41,'wifi4','Wifi 4','uploaded/icons/2014-09-09/2014-09-09-13-32-48_wifi4.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (42,'wifi5','Wifi 5','uploaded/icons/2014-09-09/2014-09-09-13-37-16_wifi5.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (43,'wifi6','Wifi 6','uploaded/icons/2014-09-09/2014-09-09-13-38-03_wifi6.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (44,'wifi7','Wifi 7','uploaded/icons/2014-09-09/2014-09-09-13-38-21_wifi7.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (45,'wifi8','Wifi 8','uploaded/icons/2014-09-09/2014-09-09-13-38-41_wifi8.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (46,'wifi9','Wifi 9','uploaded/icons/2014-09-09/2014-09-09-13-39-16_wifi9.png');
+INSERT INTO `inventory_iconsettings` (`id`,`name`,`alias`,`upload_image`) VALUES (47,'wifi10','Wifi 10','uploaded/icons/2014-09-09/2014-09-09-13-39-37_wifi10.png');
