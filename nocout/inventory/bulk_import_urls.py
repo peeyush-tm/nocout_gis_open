816afbf1df2efa8dc54ff19e7713297d89c8f110
Priyesh <priyesh@teramatrix.co>
2014-09-18 19:16:29 +0530

    Adding urls for gis inventory bulk uploads detail view.

--- nocout/inventory/bulk_import_urls.py
+++ nocout/inventory/bulk_import_urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, url
 from inventory import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.GISInventoryBulkImport.as_view(), name='gis_inventory_bulk_import'),
-  url(r'^gis_inventory/$', views.GISInventoryBulkImport.as_view(), name='gis_inventory_bulk_import'),
-  url(r'^gis_inventory_validator/$', views.GISInventoryBulkImport.as_view(), name='gis_inventory_validator'),
+  url(r'^$', views.GISInventoryBulkImportList.as_view(), name='gis_inventory_bulk_import_list'),
+  url(r'^gis_inventory/$', views.GISInventoryBulkImportView.as_view(), name='gis_inventory_bulk_import'),
+  url(r'^gis_inventory_validator/$', views.GISInventoryBulkImportView.as_view(), name='gis_inventory_validator'),
+    url(r'^gisinventorybulkimportlistingtable/', views.GISInventoryBulkImportListingTable.as_view(), name='GISInventoryBulkImportListingTable'),
   # url(r'^gis_inventory_bulk_processing/$', views.GISInventoryBulkProcessing.as_view(), name='gis_inventory_bulk_processing')
 )
