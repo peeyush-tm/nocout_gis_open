b456c01ffaf0d1a340b79a5af45e8170eabb516b
yogender-tm <yogender@teramatrix.co>
2014-09-26 01:48:41 +0530

    Performance issue resolved

--- nocout/devicevisualization/static/js/devicevisualization.js
+++ nocout/devicevisualization/static/js/devicevisualization.js
@@ -17,11 +17,11 @@ if(window.location.origin) {
 
 /*Set cookies if not exist*/
 if(!$.cookie("isFreezeSelected")) {
-    $.cookie("isFreezeSelected", 0, {path: '/', secure: true});
+    $.cookie("isFreezeSelected", 0, {path: '/', secure : true});
 }
 
 if(!$.cookie("freezedAt")) {
-    $.cookie("freezedAt", 0, {path: '/', secure: true});
+    $.cookie("freezedAt", 0, {path: '/', secure : true});
 }
 
 /*Save cookie value to variable*/
