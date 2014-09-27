3a243152c5f551b91c2d45155ff4401a0083a435
Peeyush <praj@teramatrix.in>
2014-09-27 22:28:37 +0530

    naming changes and customer addition

--- nocout/alert_center/views.py
+++ nocout/alert_center/views.py
@@ -83,6 +83,7 @@ def getCustomerAlertDetail(request):
         {'mData': 'current_value', 'sTitle': 'Value', 'sWidth': 'null', 'sClass': 'hidden-xs',
          'bSortable': True},
         {'mData': 'sys_timestamp', 'sTitle': 'Timestamp', 'sWidth': 'null', 'bSortable': True},
+        {'mData': 'customer_name', 'sTitle': 'Customer Name', 'sWidth': 'null', 'bSortable': True},
         {'mData': 'action', 'sTitle': 'Action', 'sWidth': 'null', 'sClass': 'hidden-xs',
          'bSortable': False},
         ]
@@ -97,9 +98,9 @@ class GetCustomerAlertDetail(BaseDatatableView):
     """
     model = EventNetwork
     columns = ['device_name', 'device_type', 'machine_name', 'site_name', 'ip_address', 'severity',
-               'current_value', 'sys_timestamp', 'description']
+               'current_value', 'sys_timestamp', 'description', 'customer_name']
     order_columns = ['device_name', 'device_type', 'machine_name', 'site_name', 'ip_address', 'severity',
-                     'current_value', 'sys_timestamp', 'description']
+                     'current_value', 'sys_timestamp', 'description', 'customer_name']
 
     def filter_queryset(self, qs):
         """
@@ -628,9 +629,9 @@ class AlertCenterNetworkListing(ListView):
         """
         context = super(AlertCenterNetworkListing, self).get_context_data(**kwargs)
         data_source=self.kwargs.get('data_source','')
-        data_source_title = data_source.title() \
-                            if data_source in ["latency", "packet_drop"] \
-                            else ("value".title() if data_source in ["service"] else "packet drop".title())
+        data_source_title = "Latency Average (ms) " \
+                            if data_source in ["latency"] \
+                            else ("value".title() if data_source in ["service"] else "packet drop (%)".title())
 
         data_tab = self.request.GET.get('data_tab','P2P')
 
@@ -877,12 +878,13 @@ class CustomerAlertList(ListView):
              'bSortable': True},
             {'mData': 'sector_id', 'sTitle': 'Sector ID', 'sWidth': 'null', 'sClass': 'hidden-xs',
              'bSortable': True},
-            {'mData': 'current_value', 'sTitle': 'Packet Drop %', 'sWidth': 'null', 'sClass': 'hidden-xs',
+            {'mData': 'current_value', 'sTitle': 'Packet Drop (%)', 'sWidth': 'null', 'sClass': 'hidden-xs',
              'bSortable': True }
              if data_source.lower() in ['down','packet_drop'] else
-            {'mData': 'current_value', 'sTitle': 'Latency Average(ms)', 'sWidth': 'null', 'sClass': 'hidden-xs',
+            {'mData': 'current_value', 'sTitle': 'Latency Average (ms) ', 'sWidth': 'null', 'sClass': 'hidden-xs',
              'bSortable': True },
             {'mData': 'sys_timestamp', 'sTitle': 'Timestamp', 'sWidth': 'null', 'bSortable': True},
+            {'mData': 'customer_name', 'sTitle': 'Customer Name', 'sWidth': 'null', 'bSortable': True},
             {'mData': 'action', 'sTitle': 'Action', 'sWidth': 'null', 'bSortable': True},
             ]
 
@@ -901,9 +903,9 @@ class CustomerAlertListingTable(BaseDatatableView):
     """
     model = EventNetwork
     columns = ['device_name', 'device_type', 'machine_name', 'site_name', 'ip_address', 'severity',
-               'current_value', 'sys_timestamp', 'description']
+               'current_value', 'sys_timestamp', 'description', 'customer_name']
     order_columns = ['device_name', 'device_type', 'machine_name', 'site_name', 'ip_address', 'severity',
-                     'current_value', 'sys_timestamp', 'description']
+                     'current_value', 'sys_timestamp', 'description', 'customer_name']
 
     def filter_queryset(self, qs):
         """
@@ -1614,6 +1616,7 @@ def prepare_alert_results(device_list, performance_data):
         circuit_objects = None
         basestation_objects = None
         sector_objects = None
+        customer_objects = None
 
         try:
             city_objects = City.objects.prefetch_related('state').get(id=device_object.city)
@@ -1634,6 +1637,8 @@ def prepare_alert_results(device_list, performance_data):
                         circuit_object = circuit_objects[0]
                         sector_objects = circuit_object.sector
                         basestation_objects = sector_objects.base_station
+                        customer_objects = circuit_object.customer
+
                 except Exception as e:
                     #database is in correct
                     # we either have multiple circuits present on the same device. that is same
@@ -1654,6 +1659,7 @@ def prepare_alert_results(device_list, performance_data):
                         'sector_id': sector_objects.sector_id if sector_objects else "N/A",
                         'city': city_objects.city_name if city_objects else "N/A",
                         'state': state_object.state_name if state_object else "N/A",
+                        'customer_name': customer_objects.alias if customer_objects else "N/A",
                         'data_source_name': data["data_source"],
                         'current_value': data["current_value"],
                         'max_value': data["max_value"],
@@ -1675,6 +1681,7 @@ def prepare_alert_results(device_list, performance_data):
                     if len(circuit_objects):
                         circuit_object = circuit_objects[0]
                         try:
+                            customer_objects = circuit_object.customer
                             sector_objects = circuit_object.sector
                             basestation_objects = sector_objects.base_station
                         except Exception as e:
@@ -1696,6 +1703,7 @@ def prepare_alert_results(device_list, performance_data):
                             'sector_id': sector_objects.sector_id if sector_objects else "N/A",
                             'city': city_objects.city_name if city_objects else "N/A",
                             'state': state_object.state_name if state_object else "N/A",
+                            'customer_name': customer_objects.alias if customer_objects else "N/A",
                             'data_source_name': data["data_source"],
                             'current_value': data["current_value"],
                             'max_value': data["max_value"],
