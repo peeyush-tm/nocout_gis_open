2c5fc6ebe2e385c2c380f00436a2c32a2f5f01bb
yogender-tm <yogender@teramatrix.co>
2014-10-17 17:31:20 +0530

    Update live polling ajax url

--- nocout/devicevisualization/static/js/devicePlottingLib.js
+++ nocout/devicevisualization/static/js/devicePlottingLib.js
@@ -2860,8 +2860,8 @@ function devicePlottingClass_gmap() {
 			}
 
 	    	$.ajax({
-				// url : base_url+"/"+"device/lp_bulk_data/?ts_template="+selected_lp_template+"&devices="+JSON.stringify(allSSIds),
-				url : base_url+"/"+"static/services.json",
+				url : base_url+"/"+"device/lp_bulk_data/?ts_template="+selected_lp_template+"&devices="+JSON.stringify(allSSIds),
+				// url : base_url+"/"+"static/services.json",
 				success : function(results) {
 
 					var result = JSON.parse(results);
