71697a1ebbf194a693aa0957bbc3a70d2cde69f3
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-10-21 19:10:24 +0530

    activity_stream.views: Modify verb_string of the Backhaul create and update method.

--- nocout/inventory/views.py
+++ nocout/inventory/views.py
@@ -815,7 +815,8 @@ class BackhaulCreate(CreateView):
         Submit the form and to log the user activity.
         """
         self.object = form.save()
-        action.send(self.request.user, verb='Created', action_object=self.object)
+        verb_string = "Create Backhaul : %s" %(self.object.alias)
+        action.send(self.request.user, verb=verb_string, action_object=self.object)
         return HttpResponseRedirect(BackhaulCreate.success_url)
 
 
@@ -844,7 +845,7 @@ class BackhaulUpdate(UpdateView):
 
         changed_fields_dict = DictDiffer(initial_field_dict, cleaned_data_field_dict).changed()
         if changed_fields_dict:
-            verb_string = 'Changed values of Backhaul : %s from initial values ' % (self.object.name) + ', '.join(
+            verb_string = 'Updated Backhaul : %s, ' % (self.object.alias) + ', '.join(
                 ['%s: %s' % (k, initial_field_dict[k]) \
                  for k in changed_fields_dict]) + \
                           ' to ' + \
