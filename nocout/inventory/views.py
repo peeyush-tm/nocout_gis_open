6a29ee5dcbc1b455c6eb792122e50657ca8f2a6f
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-10-21 19:17:17 +0530

    activity_stream.views: Modify verb_string of the SubStation create and update method.

--- nocout/inventory/views.py
+++ nocout/inventory/views.py
@@ -1429,7 +1429,8 @@ class SubStationCreate(CreateView):
         Submit the form and to log the user activity.
         """
         self.object = form.save()
-        action.send(self.request.user, verb='Created', action_object=self.object)
+        verb_string = "Create Sub Station : %s" %(self.object.alias)
+        action.send(self.request.user, verb=verb_string, action_object=self.object)
         return HttpResponseRedirect(SubStationCreate.success_url)
 
 
@@ -1456,8 +1457,8 @@ class SubStationUpdate(UpdateView):
         initial_field_dict = {field: form.initial[field] for field in form.initial.keys()}
         cleaned_data_field_dict = {field: form.cleaned_data[field] for field in form.cleaned_data.keys()}
         changed_fields_dict = DictDiffer(initial_field_dict, cleaned_data_field_dict).changed()
-        if changed_fields_dict:
-            verb_string = 'Changed values of SubStation : %s from initial values ' % (self.object.name) + ', '.join(
+        if changed_fields_dict: 
+            verb_string = 'Updaete Sub Station : %s, ' % (self.object.name) + ', '.join(
                 ['%s: %s' % (k, initial_field_dict[k]) \
                  for k in changed_fields_dict]) + \
                           ' to ' + \
