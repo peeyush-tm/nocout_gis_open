a7c9d98c1eff95cc784e63121b8975ccf5dea68c
Anoop Kumar Malav <anoop@finoit.com>
2014-10-21 16:56:23 +0530

    SectorList: Remove column name from datatable.
    In favour of alias column.
    
    This commit refs #576.

--- nocout/inventory/views.py
+++ nocout/inventory/views.py
@@ -883,7 +883,6 @@ class SectorList(ListView):
         """
         context = super(SectorList, self).get_context_data(**kwargs)
         datatable_headers = [
-            {'mData': 'name', 'sTitle': 'Name', 'sWidth': 'auto', },
             {'mData': 'alias', 'sTitle': 'Alias', 'sWidth': 'auto', },
             {'mData': 'bs_technology__alias', 'sTitle': 'Technology', 'sWidth': 'auto', },
             {'mData': 'sector_id', 'sTitle': 'ID', 'sWidth': 'auto', 'sClass': 'hidden-xs'},
@@ -909,9 +908,9 @@ class SectorListingTable(BaseDatatableView):
     Class based View to render Sector Data Table.
     """
     model = Sector
-    columns = ['name', 'alias', 'bs_technology__alias' ,'sector_id', 'sector_configured_on__device_name',
+    columns = ['alias', 'bs_technology__alias' ,'sector_id', 'sector_configured_on__device_name',
                'sector_configured_on_port__name', 'antenna__name', 'mrc', 'description']
-    order_columns = ['name', 'alias', 'bs_technology__alias' ,'sector_id', 'sector_configured_on__device_name',
+    order_columns = ['alias', 'bs_technology__alias' ,'sector_id', 'sector_configured_on__device_name',
                'sector_configured_on_port__name', 'antenna__name', 'mrc', 'description']
 
     def filter_queryset(self, qs):
