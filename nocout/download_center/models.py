2b4df31dec56bd8da97b2d186a1dacb17ee7f4a3
Peeyush <praj@teramatrix.in>
2014-10-25 16:41:56 +0530

    MIGRATIONS ::: For Download Center

--- nocout/download_center/models.py
+++ nocout/download_center/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ProcessedReportDetails(models.Model):
+    """
+    class for putting in processed data excel
+    """
+    report_name = models.CharField('Report Name', max_length=255)
+    report_path = models.CharField('Report Path', max_length=255)
+    created_on  = models.DateTimeField('Created On', auto_now=True, auto_now_add=True, blank=True)
+
