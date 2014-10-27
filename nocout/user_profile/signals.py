e8959d5a0c7d810358c2be01429a992d32a8b2fc
Gopal Kumar Roy <gopal.roy@finoit.com>
2014-10-27 19:31:35 +0530

    Auto Assign Thematic Setting while User is creating.

--- nocout/user_profile/signals.py
+++ nocout/user_profile/signals.py
@@ -8,9 +8,16 @@ from user_profile.models import UserProfile
 from inventory.models import ThematicSettings, UserThematicSettings
 
 
-
 @receiver(post_save, sender=UserProfile)
 def auto_assign_thematic(sender, **kwargs):
-	thematicSetting = ThematicSettings.objects.filter(name__contains='RSSI')[0]
-	m=UserThematicSettings(thematic_template=thematicSetting, user_profile=kwargs.get('instance'))
-	m.save()
+    thematic_setting_p2p=ThematicSettings.objects.filter(threshold_template__live_polling_template__technology__name='P2P', name__contains="RSSI")[:1]
+    if len(thematic_setting_p2p)>0:
+        UserThematicSettings.objects.create(thematic_template=thematic_setting_p2p[0], thematic_technology=thematic_setting_p2p[0].threshold_template.live_polling_template.technology, user_profile=kwargs.get('instance'))
+
+    thematic_setting_pmp=ThematicSettings.objects.filter(threshold_template__live_polling_template__technology__name='PMP', name__contains="RSSI")[:1]
+    if len(thematic_setting_pmp)>0:
+        UserThematicSettings.objects.create(thematic_template=thematic_setting_pmp[0],thematic_technology=thematic_setting_pmp[0].threshold_template.live_polling_template.technology, user_profile=kwargs.get('instance'))
+
+    thematic_setting_wimax=ThematicSettings.objects.filter(threshold_template__live_polling_template__technology__name='WIMAX',name__contains="RSSI")[:1]
+    if len(thematic_setting_wimax)>0:
+        UserThematicSettings.objects.create(thematic_template=thematic_setting_wimax[0],thematic_technology=thematic_setting_wimax[0].threshold_template.live_polling_template.technology, user_profile=kwargs.get('instance'))
