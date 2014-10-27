b6b89e0d7bd41b7ee5761448cec448f38bea3ae7
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-10-27 19:49:17 +0530

    ncount.static.js: Add a action_create.js file and call in the nocout mainpage.html

---
+++ nocout/nocout/static/js/actions_create.js
@@ -0,0 +1,37 @@
+function LogUserAction(module, action) {
+    var csrftoken = getCookie('csrftoken');
+    $.ajax({
+        url : "/logs/actioncreate/", 
+        type : "POST",
+        dataType: "json", 
+        data : {
+            module : module,
+            action : action,
+            csrfmiddlewaretoken : csrftoken,
+            },
+            success : function(response) {
+                console.log(response)
+            },
+            error : function(xhr,errmsg,err) {
+                alert(xhr.status + ": " + xhr.responseText);
+            }
+    });
+    return false;
+}
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
