b3430040c5d5118ce27f05d31b7a069e63b6c37d
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-10-28 12:42:52 +0530

    activity_stream.forms: Modify the UserActionForm from ModelForm to normal form.

--- nocout/activity_stream/forms.py
+++ nocout/activity_stream/forms.py
@@ -1,11 +1,5 @@
 from django import forms
-from django.forms import ModelForm
 
-from activity_stream.models import UserAction
-
-class UserActionForm(ModelForm):
+class UserActionForm(forms.Form):
     action = forms.CharField(max_length=128, required=True)
     module = forms.CharField(widget=forms.TextInput, required=True)
-    class Meta:
-        model = UserAction
-        fields = ['action', 'module']
