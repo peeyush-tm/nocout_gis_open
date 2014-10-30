f889375c4df88a1f003c4fb941678255ca34ca8e
Peeyush Raj <praj@teramatrix.in>
2014-10-30 20:43:46 +0530

    handle exception when there is no thematic present

--- nocout/user_profile/signals.py
+++ nocout/user_profile/signals.py
@@ -11,6 +11,9 @@ from inventory.models import ThematicSettings, UserThematicSettings
 
 @receiver(post_save, sender=UserProfile)
 def auto_assign_thematic(sender, **kwargs):
-	thematicSetting = ThematicSettings.objects.filter(name__contains='RSSI')[0]
-	m=UserThematicSettings(thematic_template=thematicSetting, user_profile=kwargs.get('instance'))
-	m.save()
+	try:
+		thematicSetting = ThematicSettings.objects.filter(name__contains='RSSI')[0]
+		m=UserThematicSettings(thematic_template=thematicSetting, user_profile=kwargs.get('instance'))
+		m.save()
+	except:
+		pass
