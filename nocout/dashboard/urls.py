ec7de901b11a09a44de8c562f5bf4f5130620f12
Anoop Malav <anoop.k.malav@gmail.com>
2014-11-03 00:47:33 +0530

    dashboard.urls: Add url dispatcher for dashboard settings create view.

--- nocout/dashboard/urls.py
+++ nocout/dashboard/urls.py
@@ -6,4 +6,5 @@ from dashboard import views
 urlpatterns = patterns('',
     url(r'^settings/$', views.DashbaordSettingsListView.as_view(), name='dashboard-settings'),
     url(r'^settings/table/$', views.DashbaordSettingsListingTable.as_view(), name='dashboard-settings-table'),
+    url(r'^settings/new/$', views.DashbaordSettingsCreateView.as_view(), name='dashboard-settings-new'),
 )
