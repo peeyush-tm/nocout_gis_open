0ad0533d369ebf9531f6eea23a588277dfef265c
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-11-07 15:15:30 +0530

    inventory.ajax: modify to update the antenna according to organisation while creation and updation of the basestation form.

--- nocout/inventory/ajax.py
+++ nocout/inventory/ajax.py
@@ -581,18 +581,14 @@ def update_antenna(request, option):
     out.append("<option value=''>Select</option>")
     try:
         org = Organization.objects.get(id=int(option))
-    except Organization.DoesNotExist as e:
+        antennas = Antenna.objects.filter(organization=int(option))[:50]
+        for antenna in antennas:
+            out.append("<option value={}>{}</option>".format(antenna.id, antenna) )
+        dajax.assign('#id_antenna', 'innerHTML', ''.join(out))
         return dajax.json()
+    except Organization.DoesNotExist:
+        pass
 
-    if request.user.userprofile.role.values_list( 'role_name', flat=True )[0] =='admin':
-        antennas = Antenna.objects.filter(organization__in=org.get_descendants(include_self=True))
-    else:
-        antennas = Antenna.objects.filter(organization=int(option))
-
-    for antenna in antennas:
-        out.append("<option value='#'>%s</option>" % antenna)
-    dajax.assign('#id_antenna', 'innerHTML', ''.join(out))
-    return dajax.json()
 
 @dajaxice_register(method='GET')
 def update_sector(request, option):
