e0a0ead158c60237b396deed313875d454a6930f
Anoop Kumar Malav <anoop@finoit.com>
2014-11-10 18:30:21 +0530

    clean Backhaul form.

--- nocout/inventory/forms.py
+++ nocout/inventory/forms.py
@@ -277,46 +277,7 @@ class BackhaulForm(forms.ModelForm):
             logger.info(e.message)
 
         if self.request is not None:
-            '''
-            If user submits form and returned with an error, then get selected values from POST data.
-            If user requests to edit an instance, then get instance values.
-            If user requests to create new entry, then return non-selected values [first 50 sliced values.]
-            '''
             request = self.request
-
-            if request.method == 'POST':
-                organization = request.POST.get('organization')
-                bh_configured_on = request.POST.get('bh_configured_on')
-                aggregator = request.POST.get('aggregator')
-                bh_switch = request.POST.get('bh_switch')
-                pop = request.POST.get('pop')
-            elif kwargs['instance'] is not None: # request.method == 'GET'
-                instance = kwargs['instance']
-                organization = instance.organization
-                bh_configured_on = instance.bh_configured_on.id if instance.bh_configured_on else None
-                aggregator = instance.aggregator.id if instance.aggregator else None
-                bh_switch = instance.bh_switch.id if instance.bh_switch else None
-                pop = instance.pop.id if instance.pop else None
-            else: # request.method == 'GET' and instance is None
-                organization = request.user.userprofile.organization
-                bh_configured_on = None
-                aggregator = None
-                bh_switch = None
-                pop = None
-
-            devices_set = Device.objects.values_list('id', flat=True)
-            if organization:
-                devices_set = devices_set.filter(organization=organization)
-
-            device_ids = set(list(devices_set[:50]) + [bh_configured_on, aggregator, bh_switch, pop])
-
-            if None in device_ids: device_ids.remove(None)
-            if '' in device_ids: device_ids.remove('')
-
-            self.fields['bh_configured_on'].queryset = self.fields['bh_configured_on'].queryset.filter(id__in=device_ids)
-            self.fields['aggregator'].queryset = self.fields['aggregator'].queryset.filter(id__in=device_ids)
-            self.fields['bh_switch'].queryset = self.fields['bh_switch'].queryset.filter(id__in=device_ids)
-            self.fields['pop'].queryset = self.fields['pop'].queryset.filter(id__in=device_ids)
 
             if request.user.userprofile.role.values_list( 'role_name', flat=True )[0] =='admin':
                 self.fields['organization'].queryset = request.user.userprofile.organization.get_descendants(include_self=True)
