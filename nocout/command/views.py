ce6608e7b7d30a1ec7a5143df1437bdfcfa79de0
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-11-14 19:59:55 +0530

    command.views: Change the order of the url.

--- nocout/command/views.py
+++ nocout/command/views.py
@@ -60,8 +60,8 @@ class CommandListingTable(PermissionsRequiredMixin, ValuesQuerySetMixin, Datatab
 
         json_data = [ { key: val if val else "" for key, val in dct.items() } for dct in qs ]
         for dct in json_data:
-            dct.update(actions='<a href="/command/edit/{0}"><i class="fa fa-pencil text-dark"></i></a>\
-                <a href="/command/delete/{0}"><i class="fa fa-trash-o text-danger"></i></a>'.format(dct.pop('id')))
+            dct.update(actions='<a href="/command/{0}/edit/"><i class="fa fa-pencil text-dark"></i></a>\
+                <a href="/command/{0}/delete/"><i class="fa fa-trash-o text-danger"></i></a>'.format(dct.pop('id')))
         return json_data
 
 
