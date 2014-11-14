17a9c9682cfcebf8f382045e027b26bdf7bbf8a7
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-11-14 16:30:06 +0530

    service.views: Change the order of the url.

--- nocout/service/views.py
+++ nocout/service/views.py
@@ -69,8 +69,8 @@ class ServiceListingTable(PermissionsRequiredMixin, ValuesQuerySetMixin, Datatab
         """
         json_data = [{key: val if val else "" for key, val in dct.items()} for dct in qs]
         for dct in json_data:
-            dct.update(actions='<a href="/service/edit/{0}"><i class="fa fa-pencil text-dark"></i></a>\
-                <a href="/service/delete/{0}"><i class="fa fa-trash-o text-danger"></i></a>'.format(dct.pop('id')))
+            dct.update(actions='<a href="/service/{0}/edit/"><i class="fa fa-pencil text-dark"></i></a>\
+                <a href="/service/{0}/delete/"><i class="fa fa-trash-o text-danger"></i></a>'.format(dct.pop('id')))
         return json_data
 
 
