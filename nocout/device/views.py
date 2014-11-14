b1dd31b474db035c4c40d27e40b719580cb2cdd1
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-11-14 18:56:25 +0530

    device_vendor.views: Change the order of the url.

--- nocout/device/views.py
+++ nocout/device/views.py
@@ -2316,8 +2316,8 @@ class DeviceVendorListingTable(PermissionsRequiredMixin, BaseDatatableView):
         if qs:
             qs = [{key: val if val else "" for key, val in dct.items()} for dct in qs]
         for dct in qs:
-            dct.update(actions='<a href="/vendor/edit/{0}"><i class="fa fa-pencil text-dark"></i></a>\
-                        <a href="/vendor/delete/{0}"><i class="fa fa-trash-o text-danger"></i></a>'.format(
+            dct.update(actions='<a href="/vendor/{0}/edit/"><i class="fa fa-pencil text-dark"></i></a>\
+                        <a href="/vendor/{0}/delete/"><i class="fa fa-trash-o text-danger"></i></a>'.format(
                 dct.pop('id')))
         return qs
 
