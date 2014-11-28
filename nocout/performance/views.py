e95ba351dd9488a03c5cb8c78f5cf8ed309c1935
Peeyush <praj@teramatrix.in>
2014-11-28 20:09:12 +0530

    Status Since Performance Multi Proecssing

--- nocout/performance/views.py
+++ nocout/performance/views.py
@@ -2281,7 +2281,7 @@ def get_multiprocessing_performance_data(q,device_list, machine, model):
             ).strftime("%m/%d/%y (%b) %H:%M:%S (%I:%M %p)")
 
             perf_result["age"] = datetime.datetime.fromtimestamp(
-                    float(data["age"])).strftime("%d days %H:%M:%S") if data["age"] else ""
+                    float(data["age"])).strftime("%m/%d/%y (%b) %H:%M:%S") if data["age"] else ""
 
             device_result[device] = perf_result
     # (device_result)
