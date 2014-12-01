26e1788831369b5bc48e8a1c847fc760b4a87723
nitin <nitin@teramatrix.co>
2014-12-02 01:59:27 +0530

    ETL code optimization:1

--- nocout_device_app/pollers_ETL/status/service/service_status_tables_migration.py
+++ nocout_device_app/pollers_ETL/status/service/service_status_tables_migration.py
@@ -98,7 +98,7 @@ def main(**configs):
 	    doc.get('age')
         )
         data_values.append(t)
-
+	t = ()
     if data_values:
     	insert_data(configs.get('table_name'), data_values, db,configs=configs)
    	print "Data inserted into my mysql db"
