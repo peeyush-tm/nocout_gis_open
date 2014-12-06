0abb0ba8ba47420384ad998c76c69e8621350c36
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-12-06 18:53:43 +0530

    device_type.views: Modify to create the device type using wizard.

--- nocout/device/wizard_device_type_urls.py
+++ nocout/device/wizard_device_type_urls.py
@@ -4,6 +4,7 @@ from device import views
 
 
 urlpatterns = patterns('',
+    url(r'^device-type/$', views.GisWizardDeviceTypeCreateView.as_view(), name='wizard-device-type-create'),
     url(r'^device-type/(?P<pk>\d+)/$', views.GisWizardDeviceTypeUpdateView.as_view(), name='wizard-device-type-update'),
 
     url(r'^device-type/(?P<dt_pk>\d+)/service/$', views.GisWizardServiceListView.as_view(), name='wizard-service-list'),
