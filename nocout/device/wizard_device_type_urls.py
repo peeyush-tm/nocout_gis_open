5f399560d3143021a63fe1e88d01db443cce3f63
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-12-06 17:42:14 +0530

    device.views: Remove the service data source list.

--- nocout/device/wizard_device_type_urls.py
+++ nocout/device/wizard_device_type_urls.py
@@ -11,7 +11,4 @@ urlpatterns = patterns('',
     url(r'^device-type/(?P<dt_pk>\d+)/service/(?P<pk>\d+)/$', views.GisWizardServiceUpdateView.as_view(), name='wizard-service-update'),
     url(r'^device-type/(?P<dt_pk>\d+)/service/(?P<pk>\d+)/delete/$', views.wizard_service_delete, name='wizard-service-delete'),
 
-    url(r'^device-type/(?P<dt_pk>\d+)/service/(?P<dts_pk>\d+)/data-source/$', views.GisWizardDataSourceListView.as_view(), name='wizard-data-source-list'),
-    url(r'^device-type/(?P<dt_pk>\d+)/service/(?P<dts_pk>\d+)/data-source-table/$', views.GisWizardDataSourceListing.as_view(), name='wizard-data-source-list-table'),
-    url(r'^device-type/(?P<dt_pk>\d+)/service/(?P<dts_pk>\d+)/data-source-table/(?P<pk>\d+)/delete/$', views.wizard_data_source_delete, name='wizard-data-source-delete'),
 )
