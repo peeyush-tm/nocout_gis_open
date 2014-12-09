d4612b0ad8d217adbd9155aa156ef9ff53e22dd2
Anoop Kumar Malav <anoop@finoit.com>
2014-12-09 13:11:22 +0530

    dashboard.forms: MFRDFRReportsForm: Fix styling of form.

--- nocout/dashboard/forms.py
+++ nocout/dashboard/forms.py
@@ -42,6 +42,22 @@ class MFRDFRReportsForm(forms.ModelForm):
     Render and validate form for MFRDFRReports.
     """
 
+    def __init__(self, *args, **kwargs):
+        super(MFRDFRReportsForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            if field.widget.attrs.has_key('class'):
+                if isinstance(field.widget, forms.widgets.Select):
+                    field.widget.attrs['class'] += ' col-md-12'
+                    field.widget.attrs['class'] += ' select2select'
+                else:
+                    field.widget.attrs['class'] += ' form-control'
+            else:
+                if isinstance(field.widget, forms.widgets.Select):
+                    field.widget.attrs.update({'class': 'col-md-12 select2select'})
+                else:
+                    field.widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = MFRDFRReports
         fields = ('name', 'type', 'process_for', 'upload_to')
