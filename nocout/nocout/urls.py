b4f70963d4c149a54a06e96bc57a51a02f3a2d16
Gopal Kumar Roy <gopal.roy@finoit.com>
2014-12-10 19:49:52 +0530

    nocout.urls: Added url for Escalation in nocout's url.

--- nocout/nocout/urls.py
+++ nocout/nocout/urls.py
@@ -77,6 +77,8 @@ urlpatterns = patterns('',
                        url(r'^country/', include('device.country_urls')),
                        url(r'^state/', include('device.state_urls')),
                        url(r'^city/', include('device.city_urls')),
+                       url(r'^wizard/', include('device.wizard_device_type_urls')),
+                       url(r'^escalation/', include('alarm_escalation.urls')),
                        )
 
 # if settings.DEBUG:
