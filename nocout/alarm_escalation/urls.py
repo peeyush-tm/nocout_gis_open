eede62610fb2ac179ed089eab149b8a0a16e5809
Gopal Kumar Roy <gopal.roy@finoit.com>
2014-12-12 20:04:29 +0530

    updated urls of alarm_escalation.

--- nocout/alarm_escalation/urls.py
+++ nocout/alarm_escalation/urls.py
@@ -3,12 +3,12 @@ from alarm_escalation import views
 
 
 urlpatterns = patterns('',
-                       url(r'^$', views.EscalationList.as_view(), name='escalation_list'),
-                       url(r'^new/', views.AlarmEscalationCreate.as_view(), name='escalation_create'),
-                       url(r'^(?P<pk>\d+)/edit/$', views.AlarmEscalationUpdate.as_view(), name='escalation_update'),
-                       url(r'^(?P<pk>\d+)/delete/$', views.AlarmEscalationDelete.as_view(), name='escalation_delete'),
-                       url(r'^escalationlistingtable/', views.EscalationListingTable.as_view(),
-                           name='EscalationListingTable'),
+                       #url(r'^$', views.EscalationList.as_view(), name='escalation_list'),
+                       #url(r'^new/', views.AlarmEscalationCreate.as_view(), name='escalation_create'),
+                       #url(r'^(?P<pk>\d+)/edit/$', views.AlarmEscalationUpdate.as_view(), name='escalation_update'),
+                       #url(r'^(?P<pk>\d+)/delete/$', views.AlarmEscalationDelete.as_view(), name='escalation_delete'),
+                       #url(r'^escalationlistingtable/', views.EscalationListingTable.as_view(),
+                           #name='EscalationListingTable'),
                        url(r'^level/$', views.LevelList.as_view(), name='level_list'),
                        url(r'^level/new/', views.LevelCreate.as_view(), name='level_create'),
                        url(r'^level/(?P<pk>\d+)/edit/$', views.LevelUpdate.as_view(), name='level_update'),
