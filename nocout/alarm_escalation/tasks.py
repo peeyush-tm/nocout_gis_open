cdfcc5e9ab1951cbe937ec7aa41efca08b23d0f5
Gopal Kumar Roy <gopal.roy@finoit.com>
2014-12-12 12:13:04 +0530

    alarm_escalation.tasks: Added tasks file in alarm_escalation.

--- nocout/alarm_escalation/tasks.py
+++ nocout/alarm_escalation/tasks.py
@@ -12,9 +12,9 @@ from alarm_escalation.models import AlarmEscalation
 from organization.models import Organization
 
 @task
-def raise_alarms():
+def raise_alarms_for_bad_performance():
     """
-    Raises alarms.
+    Raises alarms for bad performance of device.
     """
     for org in Organization.objects.all():
         for alarm in AlarmEscalation.objects.filter(is_closed=False, level__organization=org):
@@ -22,31 +22,77 @@ def raise_alarms():
             level = alarm.get_level(age.seconds)
 
             if getattr(alarm, 'l%d_email_status' % level.name) == 0:
-                alert_emails.delay(alarm, level)
-                alert_phones.delay(alarm, level)
+                alert_emails_for_bad_performance.delay(alarm, level)
+                alert_phones_for_bad_performance.delay(alarm, level)
                 setattr(alarm, 'l%d_email_status' % level.name, 1)
                 alarm.save()
 
-
 @task
-def alert_emails(alarm, level):
+def alert_emails_for_bad_performance(alarm, level):
     """
-    Sends Emails.
+    Sends Emails for bad performance.
     """
     emails = level.get_emails()
     context_dict = {'alarm' : alarm}
     context_dict['level'] = level
     subject = render_to_string('alarm_message/subject.txt', context_dict)
     subject = ''.join(subject.splitlines())
-    message = render_to_string('alarm_message/message.html', context_dict)
+    message = render_to_string('alarm_message/bad_message.html', context_dict)
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, emails, fail_silently=False)
 
 @task
-def alert_phones(alarm, level):
+def alert_phones_for_bad_performance(alarm, level):
     """
-    Sends sms to phones.
+    Sends sms to phones for bad performance.
     """
     phones = level.get_phones()
     # message = ''
     # send_sms(subject, message, settings.DEFAULT_FROM_PHONE, phones, fail_silently=False)
     pass
+
+@task
+def raise_alarms_for_good_performance():
+    """
+    Raises alarms for good performance of device.
+    """
+    for org in Organization.objects.all():
+        for alarm in AlarmEscalation.objects.filter(is_closed=False, level__organization=org):
+            age = timezone.now() - alarm.status_since
+            level = alarm.get_level(age.seconds)
+            if getattr(alarm, 'l%d_email_status' % level.name) == 0:
+                alert_emails_for_good_performance.delay(alarm, level)
+                alert_phones_for_good_performance.delay(alarm, level)
+                alarm.is_closed = True
+
+
+@task
+def alert_emails_for_good_performance(alarm, level):
+    """
+    Sends Emails for good performance.
+    """
+    emails = level.get_emails()
+    context_dict = {'alarm' : alarm}
+    context_dict['level'] = level
+    subject = render_to_string('alarm_message/subject.txt', context_dict)
+    subject = ''.join(subject.splitlines())
+    message = render_to_string('alarm_message/good_message.html', context_dict)
+    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, emails, fail_silently=False)
+
+@task
+def alert_phones_for_good_performance(alarm, level):
+    """
+    Sends sms to phones for good performance.
+    """
+    phones = level.get_phones()
+    # message = ''
+    # send_sms(subject, message, settings.DEFAULT_FROM_PHONE, phones, fail_silently=False)
+    pass
+
+def check_device_status():
+    """
+    Will check the status of devices of organizations.
+    """
+    for org in Organization.objects.all():
+        devices = org.device_set.all()
+        for device in devices:
+            machine_name = device.machine.name
