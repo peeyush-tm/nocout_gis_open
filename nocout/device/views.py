4b49e2798a7988c3758e57680e4e8c6fe25f4aca
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-12-16 15:49:08 +0530

    device.view: modify view to exclude the overlappin events devices.

--- nocout/device/views.py
+++ nocout/device/views.py
@@ -10,6 +10,7 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from django.core.urlresolvers import reverse_lazy, reverse
+from datetime import datetime
 from device.models import Device, DeviceType, DeviceTypeFields, DeviceTypeFieldsValue, DeviceTechnology, \
     TechnologyVendor, DeviceVendor, VendorModel, DeviceModel, ModelType, DevicePort, Country, State, City, \
     DeviceFrequency, DeviceTypeServiceDataSource, DeviceTypeService
@@ -37,6 +38,7 @@ from django.db.models import Q
 from service.forms import DTServiceDataSourceUpdateFormSet
 from inventory.utils.util import ptp_device_circuit_backhaul, organization_customer_devices, \
     organization_network_devices, organization_backhaul_devices
+from scheduling_management.models import Event
 
 
 from django.views.decorators.csrf import csrf_exempt
@@ -3527,26 +3529,31 @@ def list_schedule_device(request):
 
     sSearch = request.GET['sSearch']
     scheduling_type = request.GET['scheduling_type']
+    new_start_time = datetime.strptime(request.GET['start_on_time'], '%H:%M').time()
+    new_end_time = datetime.strptime(request.GET['end_on_time'], '%H:%M').time()
+    over_lap_event = Event.objects.exclude(Q(start_on_time__gte=new_end_time) | Q(end_on_time__lte=new_start_time))
+    over_lap_device_ids = Device.objects.filter(event__in=over_lap_event).values_list("id", flat=True)
+
     org = request.user.userprofile.organization
     device_list = Device.objects.filter(organization__in=[org],
                                         is_added_to_nms=1,
                                         is_deleted=0,)
     if scheduling_type == 'devi':
-        device = device_list.filter(device_alias__icontains=sSearch).values('id', 'device_alias')
+        device = device_list.filter(device_alias__icontains=sSearch)
     elif scheduling_type == 'dety':
         device = device_list.filter(device_type__in=DeviceType.objects.\
-                    filter(alias__icontains=sSearch).values_list('id', flat=True)).\
-                    values('id', 'device_alias')
+                    filter(alias__icontains=sSearch).values_list('id', flat=True))
     elif scheduling_type == 'cust':
         device_list = organization_customer_devices(organizations=[org], technology = None, specify_ptp_type='all')
-        device = device_list.filter(device_alias__icontains=sSearch).values('id', 'device_alias')
+        device = device_list.filter(device_alias__icontains=sSearch)
     elif scheduling_type == 'netw':
         device_list = organization_network_devices(organizations=[org], technology = None, specify_ptp_bh_type='all')
-        device = device_list.filter(device_alias__icontains=sSearch).values('id', 'device_alias')
+        device = device_list.filter(device_alias__icontains=sSearch)
     elif scheduling_type == 'back':
         device_list = organization_backhaul_devices(organizations=[org], technology = None)
-        device = device_list.filter(device_alias__icontains=sSearch).values('id', 'device_alias')
+        device = device_list.filter(device_alias__icontains=sSearch)
 
+    device = device.exclude(id__in=over_lap_device_ids).values('id', 'device_alias') # excule the overlapping devices
 
     return HttpResponse(json.dumps({
         "total_count": device.count(),
