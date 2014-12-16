f6c8131c136f5925b3d3fe0d0fa828596acc0393
Gopal Kumar Roy <gopal.roy@finoit.com>
2014-12-16 16:10:40 +0530

    alarm_escalation.tasks: update EscalationStatus severity when ServiceStatus severity updated.

--- nocout/alarm_escalation/tasks.py
+++ nocout/alarm_escalation/tasks.py
@@ -38,6 +38,7 @@ def raise_alarms(service_status_list, org):
                                             defaults={'severity': service_status.severity, 'old_status': old_status, 'new_status': new_status})
 
         age = timezone.now() - obj.status_since
+        obj.severity = service_status.severity
         level_list = obj.organization.escalationlevel_set.filter(device_type__name=obj.device_type,
                                             service__name=obj.service,
                                             service_data_source__name=obj.service_data_source)
@@ -69,32 +70,6 @@ def raise_alarms(service_status_list, org):
                 alert_phones_for_good_performance.delay(obj, escalation_level)
 
 
-# @task
-# def raise_alarms_for_bad_performance(service_status_list, org):
-#     """
-#     Raises alarms for bad performance of device.
-#     """
-#     escalation_level = None
-#     for service_status in service_status_list:
-#         device = Device.objects.get(device_name=service_status.device_name)
-#         obj, created = EscalationStatus.objects.get_or_create(organization=org,
-#                                             device_type=DeviceType.objects.get(id=device.device_type.name),
-#                                             service=service_status.service,
-#                                             service_data_source=service_status.service_data_source,
-#                                             ip=service_status.ip_address,
-#                                             severity=service_status.severity)
-#         age = timezone.now() - obj.status_since
-#         level_list = obj.organization.escalationlevel_set.all()
-#         for level in level_list:
-#             if age>=level.alarm_age:
-#                 escalation_level = level
-
-#         if getattr(obj, 'l%d_email_status' % escalation_level.name) == 0:
-#             alert_emails_for_bad_performance.delay(obj, escalation_level)
-#             alert_phones_for_bad_performance.delay(obj, escalation_level)
-#             setattr(obj, 'l%d_email_status' % escalation_level.name, 1)
-#             obj.save()
-
 @task
 def alert_emails_for_bad_performance(alarm, level):
     """
@@ -120,30 +95,6 @@ def alert_phones_for_bad_performance(alarm, level):
     pass
 
 
-# @task
-# def raise_alarms_for_good_performance(service_status_list, org):
-#     """
-#     Raises alarms for good performance of device.
-#     """
-#     for service_status in service_status_list:
-#         device = Device.objects.get(device_name=service_status.device_name)
-#     for escalation in EscalationStatus.objects.get(organization=org,
-#                                             device_type=DeviceType.objects.get(id=device.device_type.name),
-#                                             service=service_status.service,
-#                                             service_data_source=service_status.service_data_source,
-#                                             ip=service_status.ip_address,
-#                                             severity=service_status.severity)
-
-#     age = timezone.now() - escalation.status_since
-#     level_list = EscalationLevel.objects.filter(organization=escalation.organization, alarm_age=age.seconds)
-
-#     for level in level_list:
-#         if getattr(escalation, 'l%d_email_status' % level.name) == 1:
-#             alert_emails_for_good_performance.delay(escalation, level)
-#             alert_phones_for_good_performance.delay(escalation, level)
-#             escalation.save()
-
-
 @task
 def alert_emails_for_good_performance(alarm, level):
     """
@@ -154,7 +105,7 @@ def alert_emails_for_good_performance(alarm, level):
     context_dict['level'] = level
     subject = render_to_string('alarm_message/subject.txt', context_dict)
     subject = ''.join(subject.splitlines())
-    message = render_to_response('alarm_message/good_message.html', context_dict)
+    message = render_to_string('alarm_message/good_message.html', context_dict)
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, emails, fail_silently=False)
 
 
