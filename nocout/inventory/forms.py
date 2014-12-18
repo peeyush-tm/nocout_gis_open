c31e8f202da3af540f661dfd12616d5484e6096d
Anoop Kumar Malav <anoop@finoit.com>
2014-12-18 14:29:42 +0530

    inventory.templates: Fix on change of dr_site; dr_configured_on for WiMAX.

--- nocout/inventory/forms.py
+++ nocout/inventory/forms.py
@@ -1583,6 +1583,7 @@ class WizardSectorForm(SectorForm):
         self.technology = kwargs.pop('technology')
         super(WizardSectorForm, self).__init__(*args, **kwargs)
 
+        self.fields['dr_configured_on'].widget = forms.HiddenInput()
         if self.technology == 'P2P':
             self.fields['sector_configured_on'].label = 'Near End IP'
 
