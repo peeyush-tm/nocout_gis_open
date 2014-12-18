9a4084c2b3206738a0972cd4cac01a6509aebb98
Gopal Kumar Roy <gopal.roy@finoit.com>
2014-12-18 12:43:57 +0530

    alarm_escalation.forms: Fixed validation error.

--- nocout/alarm_escalation/forms.py
+++ nocout/alarm_escalation/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+from organization.models import Organization
 from alarm_escalation.models import EscalationLevel
 import logging
 logger = logging.getLogger(__name__)
@@ -13,6 +14,7 @@ class EscalationLevelForm(forms.ModelForm):
     """
 
     def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
         initial = kwargs.setdefault('initial',{})
 
         try:
@@ -27,6 +29,9 @@ class EscalationLevelForm(forms.ModelForm):
         self.fields['device_type'].empty_label = 'Select'
         self.fields['service_data_source'].empty_label = 'Select'
         self.fields['alarm_age'].widget.attrs['placeholder'] = 'Enter Age in Seconds.'
+        self.fields['service_data_source'].widget.attrs['id'] = 'id_data_source'
+        if not self.request.user.is_superuser:
+            self.fields['organization'].queryset = Organization.objects.filter(id=self.request.user.userprofile.organization.id)
         for name, field in self.fields.items():
             if field.widget.attrs.has_key('class'):
                 if isinstance(field.widget, forms.widgets.Select):
@@ -63,8 +68,13 @@ class EscalationLevelForm(forms.ModelForm):
                                                name=self.cleaned_data['name'],
                                                service=self.cleaned_data['service'],
                                                device_type=self.cleaned_data['device_type'],
-                                               service_data_source=self.cleaned_data['service_data_source']
-                                               )
+                                               service_data_source=self.cleaned_data['service_data_source'])
+            try:
+                if self.id:
+                    level_list = level_list.exclude(pk=self.id)
+            except Exception as e:
+                logger.info(e.message)
+
         if level_list:
             raise ValidationError('This Escalation level is already exists.')
         return self.cleaned_data
