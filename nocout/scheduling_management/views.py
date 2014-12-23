b4587990f1d0a478d8b9cb6d8b73fd35fbe7581c
Ashish Goyal <ashish.goyal@finoit.co.in>
2014-12-23 15:11:08 +0530

    scheduling_management.views: modify to create and update the event by clicking on calendar.

--- nocout/scheduling_management/views.py
+++ nocout/scheduling_management/views.py
@@ -133,8 +133,9 @@ class EventCreate(PermissionsRequiredMixin, CreateView):
         self.object = None
         form_class = self.get_form_class()
         form = self.get_form(form_class)
+        title = self.request.GET.get('title', None)
         return self.render_to_response(
-            self.get_context_data(form=form))
+            self.get_context_data(form=form, title=title))
 
     def post(self, request, *args, **kwargs):
         self.object = None
