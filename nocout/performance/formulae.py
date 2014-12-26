b2e5b995a23c74a852df300930ca2d3c72a7af5f
Peeyush <praj@teramatrix.in>
2014-12-26 18:18:24 +0530

    New Formula : display_time time ticks to human readable format

--- nocout/performance/formulae.py
+++ nocout/performance/formulae.py
@@ -15,3 +15,28 @@ def rta_null(rta=0):
         return None
 
     return rta
+
+
+def display_time(seconds, granularity=4):
+        """
+
+        :param seconds: seconds on float
+        :param granularity:
+        :return:
+        """
+        intervals = (
+            ('weeks', 604800),
+            ('days', 86400),
+            ('hours', 3600),
+            ('minutes', 60),
+            ('seconds', 1),
+        )
+        result = []
+        for name, count in intervals:
+            value = seconds // count
+            if value:
+                seconds -= value * count
+                if value == 1:
+                    name = name.rstrip('s')
+                result.append("{} {}".format(value, name))
+        return ', '.join(result[:granularity])
