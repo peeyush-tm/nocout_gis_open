963dae0da2db48fa04dc13cfe209a7e4b13411a8
Anoop Kumar Malav <anoop@finoit.com>
2015-01-12 11:47:47 +0530

    Add setting LOGIN_REDIRECT_URL to remove unrequired redirect.
    Previously :
    
        '/auth/' --> '/' --> '/home/'
    
    Now :
    
        '/auth/' --> '/home/'

--- nocout/nocout/settings.py
+++ nocout/nocout/settings.py
@@ -57,6 +57,7 @@ LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
 
 LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/home/'
 LOGIN_EXEMPT_URLS = (r'auth/', 'login/', 'admin/', 'sm/dialog_for_page_refresh/', 'sm/dialog_expired_logout_user/', 'reset-cache/')
 
 # If you set this to False, Django will make some optimizations so as not
