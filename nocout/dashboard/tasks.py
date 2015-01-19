84b1cdb20613c16ee05288a7e5f643f9fbc850a5
Anoop Kumar Malav <anoop@finoit.com>
2015-01-19 13:35:49 +0530

    fixes

--- nocout/dashboard/tasks.py
+++ nocout/dashboard/tasks.py
@@ -8,14 +8,14 @@ from nocout.settings import PMP, WiMAX
 from organization.models import Organization
 from device.models import DeviceTechnology, Device
 from capacity_management.models import SectorCapacityStatus
-from performance.models import Topology
+from performance.models import Topology, NetworkStatus
 from dashboard.models import (DashboardSetting, DashboardSeverityStatusTimely, DashboardSeverityStatusHourly,
         DashboardSeverityStatusDaily, DashboardSeverityStatusWeekly, DashboardSeverityStatusMonthly,
         DashboardSeverityStatusYearly, DashboardRangeStatusTimely, DashboardRangeStatusHourly, DashboardRangeStatusDaily,
         DashboardRangeStatusWeekly, DashboardRangeStatusMonthly, DashboardRangeStatusYearly,
     )
 
-from inventory.utils.util import organization_sectors
+from inventory.utils.util import organization_sectors, organization_network_devices, prepare_machines
 from dashboard.utils import get_topology_status_results, get_dashboard_status_range_counter
 
 
@@ -33,21 +33,21 @@ def calculate_timely_main_dashboard():
 
     user_organizations = Organization.objects.all()
 
-    calculate_timely_network_alert(user_organizations, dashboard_name='latency-pmp', technology=PMP)
-    calculate_timely_network_alert(user_organizations, dashboard_name='latency-wimax', technology=WiMAX)
-    calculate_timely_network_alert(user_organizations, dashboard_name='latency-network')
+    calculate_timely_latency(user_organizations, dashboard_name='latency-pmp', technology=PMP)
+    calculate_timely_latency(user_organizations, dashboard_name='latency-wimax', technology=WiMAX)
+    calculate_timely_latency(user_organizations, dashboard_name='latency-network')
 
-    calculate_timely_network_alert(user_organizations, dashboard_name='packetloss-pmp', technology=PMP)
-    calculate_timely_network_alert(user_organizations, dashboard_name='packetloss-wimax', technology=WiMAX)
-    calculate_timely_network_alert(user_organizations, dashboard_name='packetloss-network')
+    calculate_timely_packet_drop(user_organizations, dashboard_name='packetloss-pmp', technology=PMP)
+    calculate_timely_packet_drop(user_organizations, dashboard_name='packetloss-wimax', technology=WiMAX)
+    calculate_timely_packet_drop(user_organizations, dashboard_name='packetloss-network')
 
-    calculate_timely_network_alert(user_organizations, dashboard_name='down-pmp', technology=PMP)
-    calculate_timely_network_alert(user_organizations, dashboard_name='down-wimax', technology=WiMAX)
-    calculate_timely_network_alert(user_organizations, dashboard_name='down-network')
+    calculate_timely_down_status(user_organizations, dashboard_name='down-pmp', technology=PMP)
+    calculate_timely_down_status(user_organizations, dashboard_name='down-wimax', technology=WiMAX)
+    calculate_timely_down_status(user_organizations, dashboard_name='down-network')
 
-    calculate_timely_network_alert(user_organizations, dashboard_name='temperature', technology=PMP)
-    calculate_timely_network_alert(user_organizations, dashboard_name='temperature', technology=WiMAX)
-    calculate_timely_network_alert(user_organizations, dashboard_name='temperature')
+    calculate_timely_temperature(user_organizations, chart_type='IDU')
+    calculate_timely_temperature(user_organizations, chart_type='ACB')
+    calculate_timely_temperature(user_organizations, chart_type='FAN')
 
 
 def calculate_timely_sector_capacity(technology, model, created_on):
@@ -132,22 +132,93 @@ def calculate_timely_sales_opportunity(technology, model, created_on):
     model.objects.bulk_create(data_list)
 
 
-def calculate_timely_network_alert(organizations, dashboard_name, technology=None):
+def calculate_timely_latency(organizations, dashboard_name, technology=None):
+
+    sector_devices = organization_network_devices(organizations, technology.ID)
+    sector_devices = sector_devices.filter(sector_configured_on__isnull=True).values('machine_name', 'device_name')
+
+    machine_dict = prepare_machines(sector_devices)
+    status_dict_list = []
+    for machine_name, device_list in machine_dict.items():
+        status_dict_list += NetworkStatus.objects.order_by('device_name').filter(device_name__in=device_list,
+                service_name='ping',
+                data_source='rta',
+                severity__in=['warning', 'critical', 'down']
+            ).exclude(current_value=100).using(machine_name).values()
+
+    calculate_timely_network_alert(dashboard_name, technology, status_dict_list)
+
+
+def calculate_timely_packet_drop(organizations, dashboard_name, technology=None):
+
+    sector_devices = organization_network_devices(organizations, technology.ID)
+    sector_devices = sector_devices.filter(sector_configured_on__isnull=True).values('machine_name', 'device_name')
+
+    machine_dict = prepare_machines(sector_devices)
+    status_dict_list = []
+    for machine_name, device_list in machine_dict.items():
+        status_dict_list += NetworkStatus.objects.order_by('device_name').filter(device_name__in=device_list,
+                service_name='ping',
+                data_source='pl',
+                severity__in=['warning', 'critical', 'down'],
+                current_value__lt=100
+            ).using(machine_name).values()
+
+    calculate_timely_network_alert(dashboard_name, technology, status_dict_list)
+
+
+def calculate_timely_down_status(organizations, dashboard_name, technology=None):
+
+    sector_devices = organization_network_devices(organizations, technology.ID)
+    sector_devices = sector_devices.filter(sector_configured_on__isnull=True).values('machine_name', 'device_name')
+
+    machine_dict = prepare_machines(sector_devices)
+    status_dict_list = []
+    for machine_name, device_list in machine_dict.items():
+        status_dict_list += NetworkStatus.objects.order_by('device_name').filter(device_name__in=device_list,
+                service_name='ping',
+                data_source='pl',
+                severity__in=['down'],
+                current_value__gte=100
+            ).using(machine_name).values()
+
+    calculate_timely_network_alert(dashboard_name, technology, status_dict_list)
+
+
+def calculate_timely_temperature(organizations, chart_type='IDU'):
+
+    sector_devices = organization_network_devices(organizations, WiMAX.ID)
+    sector_devices = sector_devices.filter(sector_configured_on__isnull=True).values('machine_name', 'device_name')
+
+    if chart_type == 'IDU':
+        service_list = ['wimax_bs_temperature_acb', 'wimax_bs_temperature_fan']
+        data_source_list = ['acb_temp', 'fan_temp']
+    elif chart_type == 'ACB':
+        service_list = ['wimax_bs_temperature_acb']
+        data_source_list = ['acb_temp']
+    elif chart_type == 'FAN':
+        service_list = ['wimax_bs_temperature_fan']
+        data_source_list = ['fan_temp']
+
+    machine_dict = prepare_machines(sector_devices)
+    status_dict_list = []
+    for machine_name, device_list in machine_dict.items():
+        status_dict_list += NetworkStatus.objects.order_by('device_name').filter(device_name__in=device_list,
+                service_name__in=service_list,
+                data_source__in=data_source_list,
+                severity__in=['warning', 'critical']
+            ).using(machine_name).values()
+
+    calculate_timely_network_alert('temperature', WiMAX, status_dict_list)
+
+
+def calculate_timely_network_alert(dashboard_name, technology=None):
 
     try:
         dashboard_setting = DashboardSetting.objects.get(technology_id=technology.ID,
                 page_name='main_dashboard', name=dashboard_name, is_bh=False)
     except DashboardSetting.DoesNotExist as e:
         return None
-
-    sector_devices = organization_network_devices(organizations, technology.ID)
-    sector_devices = sector_devices.filter(sector_configured_on__isnull=True).values_list('device_name', flat=True)
-
-    status_dict_list = NetworkStatus.objects.order_by('device_name').filter(device_name__in=sector_devices,
-            service_name__in=['ping'],
-            data_source__in=['rta'],
-            severity__in=['warning', 'critical', 'down']
-        ).values()
 
     data_list = []
     device_name = ''
