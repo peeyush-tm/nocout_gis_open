3ba2835ae521379d92c42977304f2feaf51f045b
nishant <nishant@teramatrix.co>
2015-02-19 21:09:12 +0530

    Update single device alert page url

--- nocout/inventory/views.py
+++ nocout/inventory/views.py
@@ -4892,7 +4892,7 @@ def getSearchData(request, search_by="default", pk=0):
                     )
                     # Single Device alert page url
                     alert_page_url = reverse(
-                        'SingleDeviceDetails',
+                        'SingleDeviceAlertsInit',
                         kwargs={'page_type': 'customer', 'device_id' : query_result[0].id, 'service_name' : 'ping'},
                         current_app='alert_center'
                     )
@@ -4924,7 +4924,7 @@ def getSearchData(request, search_by="default", pk=0):
 
                     # Single Device alert page url
                     alert_page_url = reverse(
-                        'SingleDeviceDetails',
+                        'SingleDeviceAlertsInit',
                         kwargs={'page_type': 'customer', 'device_id' : ss_device_id, 'service_name' : 'ping'},
                         current_app='alert_center'
                     )
@@ -4964,7 +4964,7 @@ def getSearchData(request, search_by="default", pk=0):
                     )
                     # Single Device alert page url
                     alert_page_url = reverse(
-                        'SingleDeviceDetails',
+                        'SingleDeviceAlertsInit',
                         kwargs={'page_type': page_type, 'device_id' : query_result[0].id, 'service_name' : 'ping'},
                         current_app='alert_center'
                     )
