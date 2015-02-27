26f50fc2074ce9b7596f83dee83bf1067dcaa835
Priyesh-Teramatrix <priyesh@teramatrix.co>
2015-02-27 19:09:32 +0530

    GIS Perf backhaul issue resolved.

--- nocout/devicevisualization/views.py
+++ nocout/devicevisualization/views.py
@@ -1717,33 +1717,27 @@ class GISPerfData(View):
 
         # pl
         try:
-            pl_dict['value'] = NetworkStatus.objects.filter(device_name=bh_device,
+            pl_dict['value'] = NetworkStatus.objects.filter(device_name=bh_device.device_name,
                                                             data_source='pl').values('current_value').using(
-                                                            alias=bh_device.machine.name)[0].current_value
+                                                            alias=bh_device.machine.name)[0].get('current_value')
         except Exception as e:
             pl_dict['value'] = "NA"
-            # logger.error("PL not exist for backhaul device ({}). Exception: ".format(bh_device.device_name,
-            #                                                                         e.message))
 
         # rta
         try:
-            rta_dict['value'] = NetworkStatus.objects.filter(device_name=bh_device,
+            rta_dict['value'] = NetworkStatus.objects.filter(device_name=bh_device.device_name,
                                                             data_source='rta').values('current_value').using(
-                                                            alias=bh_device.machine.name)[0].current_value
+                                                            alias=bh_device.machine.name)[0].get('current_value')
         except Exception as e:
             rta_dict['value'] = "NA"
-            # logger.error("RTA not exist for backhaul device ({}). Exception: ".format(bh_device.device_name,
-            #                                                                          e.message))
 
         # bh severity
         try:
-            backhaul_data['bhSeverity'] = NetworkStatus.objects.filter(device_name=bh_device,
+            backhaul_data['bhSeverity'] = NetworkStatus.objects.filter(device_name=bh_device.device_name,
                                                             data_source='pl').values('severity').using(
-                                                            alias=bh_device.machine.name)[0].severity
+                                                            alias=bh_device.machine.name)[0].get('severity')
         except Exception as e:
             backhaul_data['bhSeverity'] = 'unknown'
-            # logger.error("BH Severity not exist for backhaul device ({}). Exception: ".format(bh_device.device_name,
-            #                                                                                  e.message))
 
         # append 'pl_dict' to 'bh_info' list
         backhaul_data['bh_info'].append(pl_dict)
