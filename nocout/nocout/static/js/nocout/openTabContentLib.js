3407a323f8588ead4458d7ce2d2b909f092b8252
nishant <nishant@teramatrix.co>
2015-03-19 13:16:51 +0530

    Sector Status & Sector Augment Alert listing divided in 3 tabs(All,PMP & WiMAX)

--- nocout/nocout/static/js/nocout/openTabContentLib.js
+++ nocout/nocout/static/js/nocout/openTabContentLib.js
@@ -40,46 +40,34 @@ $(".nav-tabs li a").click(function (e, isFirst) {
     }
 
     var destroy = false,
-        div_id = e.currentTarget.href.split("#")[1],
-        table_id = $("#" + div_id).find("table")[0].id,
-        ajax_url = e.currentTarget.attributes.data_url.value,
-        grid_headers = JSON.parse(e.currentTarget.attributes.data_header.value),
+        div_id = e.currentTarget.href.split("#").length > 1 ? e.currentTarget.href.split("#")[1] : "",
+        table_id = $("#" + div_id).find("table").length > 0 ? $("#" + div_id).find("table")[0].id : "",
+        ajax_url = e.currentTarget.attributes.data_url ? e.currentTarget.attributes.data_url.value : "",
+        grid_headers = e.currentTarget.attributes.data_header ? JSON.parse(e.currentTarget.attributes.data_header.value) : "",
         isTab = $('.nav li.active .hidden-inline-mobile');
 
-    if (last_clicked_tab != e.currentTarget.id || second_condition) {
-        var tab_id = table_id ? table_id.toLowerCase() : "";
+    if(table_id && ajax_url && grid_headers) {
+        if (last_clicked_tab != e.currentTarget.id || second_condition) {
+            var tab_id = table_id ? table_id.toLowerCase() : "";
 
-        var isPtp = ptp_list.filter(function(list_val) {
-                return tab_id.search(list_val) > -1
-            }).length,
-            pmpLength = pmp_list.filter(function(list_val) {
-                return tab_id.search(list_val) > -1
-            }).length,
-            wimaxLength = wimax_list.filter(function(list_val) {
-                return tab_id.search(list_val) > -1
-            }).length,
-            isPmpWimax = pmpLength + wimaxLength,
-            isOther = other_list.filter(function(list_val) {
-                return tab_id.search(list_val) > -1
-            }).length;
-        // If tab is ptp
-        if(isPtp > 0) {
-            for (var i = 0; i < grid_headers.length; i++) {
-                var column = grid_headers[i];
-                if (column.mData.indexOf("sector_id") > -1) {
-                    if (column.bVisible) {
-                        column.sClass = "hide";
-                    } else {
-                        column["sClass"] = "hide";
-                    }
-                }
-            }
-        // If tab is PMP or Wimax
-        } else if(isPmpWimax > 0) {
-            if(window.location.href.search("customer_live") == -1 && window.location.href.search("customer_detail") == -1) {
+            var isPtp = ptp_list.filter(function(list_val) {
+                    return tab_id.search(list_val) > -1
+                }).length,
+                pmpLength = pmp_list.filter(function(list_val) {
+                    return tab_id.search(list_val) > -1
+                }).length,
+                wimaxLength = wimax_list.filter(function(list_val) {
+                    return tab_id.search(list_val) > -1
+                }).length,
+                isPmpWimax = pmpLength + wimaxLength,
+                isOther = other_list.filter(function(list_val) {
+                    return tab_id.search(list_val) > -1
+                }).length;
+            // If tab is ptp
+            if(isPtp > 0) {
                 for (var i = 0; i < grid_headers.length; i++) {
                     var column = grid_headers[i];
-                    if (column.mData.indexOf("circuit_id") > -1 || column.mData.indexOf("customer_name") > -1) {
+                    if (column.mData.indexOf("sector_id") > -1) {
                         if (column.bVisible) {
                             column.sClass = "hide";
                         } else {
@@ -87,69 +75,86 @@ $(".nav-tabs li a").click(function (e, isFirst) {
                         }
                     }
                 }
-            }
-        // If tab is other devices
-        } else if(isOther > 0) {
-            // For other case
-            for (var i = 0; i < grid_headers.length; i++) {
-                var column = grid_headers[i],
-                    condition1 = column.mData.indexOf("sector_id") > -1,
-                    condition2 = column.mData.indexOf("circuit_id") > -1,
-                    condition3 = column.mData.indexOf("customer_name") > -1;
+            // If tab is PMP or Wimax
+            } else if(isPmpWimax > 0) {
+                if(window.location.href.search("customer_live") == -1 && window.location.href.search("customer_detail") == -1) {
+                    for (var i = 0; i < grid_headers.length; i++) {
+                        var column = grid_headers[i];
+                        if (column.mData.indexOf("circuit_id") > -1 || column.mData.indexOf("customer_name") > -1) {
+                            if (column.bVisible) {
+                                column.sClass = "hide";
+                            } else {
+                                column["sClass"] = "hide";
+                            }
+                        }
+                    }
+                }
+            // If tab is other devices
+            } else if(isOther > 0) {
+                // For other case
+                for (var i = 0; i < grid_headers.length; i++) {
+                    var column = grid_headers[i],
+                        condition1 = column.mData.indexOf("sector_id") > -1,
+                        condition2 = column.mData.indexOf("circuit_id") > -1,
+                        condition3 = column.mData.indexOf("customer_name") > -1;
 
-                if(condition1 || condition2 || condition3) {
-                    if(column.bVisible) {
-                        column.sClass = "hide";
-                    } else {
-                        column["sClass"] = "hide";
+                    if(condition1 || condition2 || condition3) {
+                        if(column.bVisible) {
+                            column.sClass = "hide";
+                        } else {
+                            column["sClass"] = "hide";
+                        }
                     }
                 }
             }
-        }
 
-        var con1 = window.location.href.indexOf('alert_center/customer/device/') > -1,
-            con2 = window.location.href.indexOf('alert_center/network/device/') > -1;
-        // If single device alert page
-        if(con1 || con2) {
+            var con1 = window.location.href.indexOf('alert_center/customer/device/') > -1,
+                con2 = window.location.href.indexOf('alert_center/network/device/') > -1;
+            // If single device alert page
+            if(con1 || con2) {
 
-            if(table_id == 'network_alert_service_table') {
-                service_name = 'service';
-            } else if(table_id == 'network_alert_down_table') {
-                service_name = 'down';
-            } else if(table_id == 'network_alert_packet_table') {
-                service_name = 'packet_drop';
-            } else if(table_id == 'network_alert_latency_table') {
-                service_name = 'latency';
-            } else {
-                service_name = 'ping';
-            }
-
-            ajax_url = ajax_url+'?service_name='+service_name;
-
-
-            try {
-
-                if(isDateFilterApplied) {
-                    ajax_url = ajax_url+'&start_date='+startDate+'&end_date='+endDate;
+                if(table_id == 'network_alert_service_table') {
+                    service_name = 'service';
+                } else if(table_id == 'network_alert_down_table') {
+                    service_name = 'down';
+                } else if(table_id == 'network_alert_packet_table') {
+                    service_name = 'packet_drop';
+                } else if(table_id == 'network_alert_latency_table') {
+                    service_name = 'latency';
+                } else {
+                    service_name = 'ping';
                 }
-                
-                var service_status_url = "";
-                if(service_name) {
-                    if(current_device_id) {
-                        service_status_url = "/performance/servicestatus/"+service_name+"/service_data_source/pl/device/"+current_device_id+"/";
+
+                ajax_url = ajax_url+'?service_name='+service_name;
+
+
+                try {
+
+                    if(isDateFilterApplied) {
+                        ajax_url = ajax_url+'&start_date='+startDate+'&end_date='+endDate;
                     }
+                    
+                    var service_status_url = "";
+                    if(service_name) {
+                        if(current_device_id) {
+                            service_status_url = "/performance/servicestatus/"+service_name+"/service_data_source/pl/device/"+current_device_id+"/";
+                        }
+                    }
+                    // Call function to get service status
+                    if (service_status_url) {
+                        getPlServiceStatus(service_status_url);
+                    }
+                } catch(e) {
+                    // console.log(e);
                 }
-                // Call function to get service status
-                if (service_status_url) {
-                    getPlServiceStatus(service_status_url);
-                }
-            } catch(e) {
-                // console.log(e);
             }
-        }
 
-        /*Call createDataTable function to create the data table for specified dom element with given data*/
-        dataTableInstance.createDataTable(table_id, grid_headers, ajax_url, destroy);
+            /*Call createDataTable function to create the data table for specified dom element with given data*/
+            dataTableInstance.createDataTable(table_id, grid_headers, ajax_url, destroy);
+        }
+    } else {
+        /*Hide the spinner*/
+        hideSpinner();
     }
 
     setTimeout(function() {
@@ -162,8 +167,6 @@ $(".nav-tabs li a").click(function (e, isFirst) {
 
     /*Refresh the tab after every given timer. Right now it is 5 minutes*/
     timeOutId = setTimeout(function () {
-
-        $("#" + anchor_id).trigger('click', true);
-
+        $("#"+anchor_id).trigger('click', true);
     }, (+(timer) + "000"));
 });
