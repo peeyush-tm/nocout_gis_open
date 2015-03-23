db08086a07d2f7e2fd66500f293618384f398685
nishant <nishant@teramatrix.co>
2015-03-23 16:08:14 +0530

    Technology wise city charter reports show on Main & RF mai Dashboard

--- nocout/nocout/static/js/nocout/main_dashboard_script.js
+++ nocout/nocout/static/js/nocout/main_dashboard_script.js
@@ -140,6 +140,31 @@ var gauge_chart_val_style = "font-size:18px;border:1px solid #DADADA;background:
         "text" : "MFR Processed"
       }
     },
+    city_charter_tbl_ids = [
+        "ptp_city_charter_table",
+        "pmp_city_charter_table",
+        "wimax_city_charter_table"
+    ],
+    city_charter_tbl_obj = {
+        "ptp_city_charter_table" : {
+            "few_url" : "",
+            "all_url" : "",
+            "few_headers" : [],
+            "all_headers" : []
+        },
+        "pmp_city_charter_table" : {
+            "few_url" : "",
+            "all_url" : "",
+            "few_headers" : [],
+            "all_headers" : []
+        },
+        "wimax_city_charter_table" : {
+            "few_url" : "",
+            "all_url" : "",
+            "few_headers" : [],
+            "all_headers" : []
+        }
+    },
     all_charts_array = [],
     charts_info_list_chunks = [],
     dashboard_call_counter = 0;
@@ -158,6 +183,8 @@ function initDashboard() {
             initAreaCharts_dashboard(function(response) {
                 // Start Server calling with the created list of charts
                 createChartAPIChunks_dashboard(all_charts_array);
+                initCityChartersDatatables();
+                // Create Dashboard City Charter Tables
             });
         });
     });
@@ -172,13 +199,6 @@ function initSpeedometerCharts_dashboard(callback) {
     // Initialize all the speedometer(solid gauge) charts
     for(var i=0;i<solid_gauge_chart_ids.length;i++) {
         if($("#"+solid_gauge_chart_ids[i]).length > 0 && solid_gauge_url_obj[solid_gauge_chart_ids[i]]["url"]) {
-            // Get chart
-            // get_speedometer_chart(
-            //     solid_gauge_url_obj[solid_gauge_chart_ids[i]]["url"],
-            //     "#"+solid_gauge_chart_ids[i],
-            //     solid_gauge_url_obj[solid_gauge_chart_ids[i]]["text"]
-
-            // );
             if(solid_gauge_url_obj[solid_gauge_chart_ids[i]]["url"]) {
                 all_charts_array.push({
                     "url"  : solid_gauge_url_obj[solid_gauge_chart_ids[i]]["url"],
@@ -201,12 +221,6 @@ function initPieCharts_dashboard(callback) {
     // Initialize all the speedometer(solid gauge) charts
     for(var i=0;i<pie_chart_ids.length;i++) {
         if($("#"+pie_chart_ids[i]).length > 0 && pie_chart_url_obj[pie_chart_ids[i]]["url"]) {
-            // Get chart
-            // highcharts_piechart(
-            //     pie_chart_url_obj[pie_chart_ids[i]]["url"],
-            //     "#"+pie_chart_ids[i],
-            //     pie_chart_url_obj[pie_chart_ids[i]]["text"]
-            // );
             if(pie_chart_url_obj[pie_chart_ids[i]]["url"]) {
                 all_charts_array.push({
                     "url"  : pie_chart_url_obj[pie_chart_ids[i]]["url"],
@@ -229,12 +243,6 @@ function initAreaCharts_dashboard(callback) {
     // Initialize all the speedometer(solid gauge) charts
     for(var i=0;i<area_chart_ids.length;i++) {
         if($("#"+area_chart_ids[i]).length > 0 && area_chart_url_obj[area_chart_ids[i]]["url"]) {
-            // Get chart for latency of wimax.
-            // highcharts_areachart(
-            //     area_chart_url_obj[area_chart_ids[i]]["url"],
-            //     "#"+area_chart_ids[i],
-            //     area_chart_url_obj[area_chart_ids[i]]["text"]
-            // );
             if(area_chart_url_obj[area_chart_ids[i]]["url"]) {
                 all_charts_array.push({
                     "url"  : area_chart_url_obj[area_chart_ids[i]]["url"],
@@ -247,6 +255,38 @@ function initAreaCharts_dashboard(callback) {
     }
 
     callback(true);
+}
+
+/**
+ * This function initialize all the city charter datatables with few columns
+ * @method initCityChartersDatatables
+ */
+function initCityChartersDatatables() {
+
+    for(var i=0;i<city_charter_tbl_ids.length;i++) {
+        
+        var table_id = city_charter_tbl_ids[i] ? $.trim(city_charter_tbl_ids[i]) : "",
+            current_table_obj = "",
+            ajax_url = "",
+            grid_headers = "";
+
+        if(table_id && $("#"+table_id).length > 0) {
+            
+            current_table_obj = city_charter_tbl_obj[table_id];
+            ajax_url = city_charter_tbl_obj[table_id]['few_url'];
+            grid_headers = city_charter_tbl_obj[table_id]['few_headers'];
+
+            dataTableInstance.createDataTable(table_id, grid_headers, ajax_url, false);
+        }
+    }
+
+    // Remove search & row per pages from city charter tables
+    if($(".dataTables_wrapper .row .col-sm-12 .pull-right").length > 0) {
+        $(".dataTables_wrapper .row .col-sm-12 .pull-right").remove();
+    }
+    if($(".dataTables_wrapper .row .col-sm-12 .pull-left").length > 0) {
+        $(".dataTables_wrapper .row .col-sm-12 .pull-left").remove();
+    }
 }
 
 /**
@@ -723,23 +763,29 @@ $("#main_dashboard_container .box-body h5 strong i, #main_dashboard_container .b
 
     // show the loader
     showSpinner();
-
     var trends_id = e.currentTarget.id ? e.currentTarget.id : "",
-        chart_dom_id = trends_id.split("_trend")[0],
+        main_element_dom_id = trends_id.split("_trend")[0],
         trends_url = "",
+        table_ajax_url = "",
+        table_headers = "",
         window_title = "";
 
-    if(solid_gauge_chart_ids.indexOf(chart_dom_id) > -1) {
-        trends_url = solid_gauge_url_obj[chart_dom_id]["trends_url"] ? $.trim(solid_gauge_url_obj[chart_dom_id]["trends_url"]) : "";
-        window_title = solid_gauge_url_obj[chart_dom_id]["text"] ? solid_gauge_url_obj[chart_dom_id]["text"]+" - " : "";
-    } else if(area_chart_ids.indexOf(chart_dom_id) > -1) {
-        trends_url = area_chart_url_obj[chart_dom_id]["trends_url"] ? $.trim(area_chart_url_obj[chart_dom_id]["trends_url"]) : "";
-        window_title = area_chart_url_obj[chart_dom_id]["text"] ? area_chart_url_obj[chart_dom_id]["text"]+" - " : "";
-    } else if(pie_chart_ids.indexOf(chart_dom_id) > -1) {
-        trends_url = pie_chart_url_obj[chart_dom_id]["trends_url"] ? $.trim(pie_chart_url_obj[chart_dom_id]["trends_url"]) : "";
-        window_title = pie_chart_url_obj[chart_dom_id]["text"] ? pie_chart_url_obj[chart_dom_id]["text"]+" - " : "";
+    if(solid_gauge_chart_ids.indexOf(main_element_dom_id) > -1) {
+        trends_url = solid_gauge_url_obj[main_element_dom_id]["trends_url"] ? $.trim(solid_gauge_url_obj[main_element_dom_id]["trends_url"]) : "";
+        window_title = solid_gauge_url_obj[main_element_dom_id]["text"] ? solid_gauge_url_obj[main_element_dom_id]["text"]+" - " : "";
+    } else if(area_chart_ids.indexOf(main_element_dom_id) > -1) {
+        trends_url = area_chart_url_obj[main_element_dom_id]["trends_url"] ? $.trim(area_chart_url_obj[main_element_dom_id]["trends_url"]) : "";
+        window_title = area_chart_url_obj[main_element_dom_id]["text"] ? area_chart_url_obj[main_element_dom_id]["text"]+" - " : "";
+    } else if(pie_chart_ids.indexOf(main_element_dom_id) > -1) {
+        trends_url = pie_chart_url_obj[main_element_dom_id]["trends_url"] ? $.trim(pie_chart_url_obj[main_element_dom_id]["trends_url"]) : "";
+        window_title = pie_chart_url_obj[main_element_dom_id]["text"] ? pie_chart_url_obj[main_element_dom_id]["text"]+" - " : "";
     } else {
-        // pass
+        var table_info_obj = city_charter_tbl_obj[main_element_dom_id];
+        if(table_info_obj) {
+            table_headers = table_info_obj['all_headers'];
+            table_ajax_url = table_info_obj['all_url'];
+            window_title = $(this).attr("title") ? $.trim($(this).attr("title")) : "City Charter Trends";
+        }
     }
 
     if(trends_url) {
@@ -811,7 +857,34 @@ $("#main_dashboard_container .box-body h5 strong i, #main_dashboard_container .b
 
         });
     } else {
-        // hide the loader
-        hideSpinner();
+            
+        if(table_headers && table_ajax_url) {
+
+            var popup_html = "";
+
+            popup_html = "<div style='overflow:auto;'>\
+                         <table id='"+main_element_dom_id+"_trends_table' class='datatable table \
+                         table-striped table-bordered table-hover'>\
+                         <thead></thead>\
+                         <tbody></tbody>\
+                         </table></div>";
+
+            /*Call the bootbox to show the popup with datatable*/
+            bootbox.dialog({
+                message: popup_html,
+                title: '<i class="fa fa-signal">&nbsp;</i> '+window_title
+            });
+
+
+            // Update Modal width to 90%;
+            $(".modal-dialog").css("width","90%");
+
+            dataTableInstance.createDataTable(main_element_dom_id+"_trends_table", table_headers, table_ajax_url, false);
+        } else {
+            // hide the loader
+            hideSpinner();
+        }
+
     }
+
 });
