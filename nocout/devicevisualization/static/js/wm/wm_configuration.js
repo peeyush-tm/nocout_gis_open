45844c1d741e6f3e7c29b1680556dd863c566a2d
nishant <nishant@teramatrix.co>
2015-04-01 20:12:14 +0530

    Manage white map configuration from settings.py

--- nocout/devicevisualization/static/js/wm/wm_configuration.js
+++ nocout/devicevisualization/static/js/wm/wm_configuration.js
@@ -1,9 +1,17 @@
 var whiteMapSettings = {
-	format: 'image/png',
 	domElement: 'wmap_container',
-	geoserver_url_India: "http://i.codescape.in:887/geoserver/wms",
-	layer: "Test2:IND_adm1",
-	initial_bounds: [68.498, 7.925, 97.335, 35.501],
+	/********************* Configuration for Localhost ***************************/
+	// format: 'image/png',
+	// geoserver_url_India: "http://i.codescape.in:887/geoserver/wms",
+	// layer: "Test2:IND_adm1",
+	// initial_bounds: [68.498, 7.925, 97.335, 35.501],
+	/********************* END ***************************/
+	/********************* Configuration for UAT(10.133.12.163) ***************************/
+	format:'application/openlayers',
+	geoserver_url_India: "http://10.133.12.163:5008/geoserver/cite/wms",
+	layer: "cite:STATE",
+	initial_bounds:[68.14339447036186,6.748584270488672,97.40963745103579,37.07349395945833],
+	/********************* END ***************************/
 	projection: "EPSG:4326",
 	maxResolution: 0.11264453125,
 	units: 'degrees',
