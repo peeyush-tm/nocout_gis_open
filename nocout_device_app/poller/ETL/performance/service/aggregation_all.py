30d9b5ef5f887f1d87bf24a731b3554a779715b9
pardeep-tm <pardeep.singh@teramatrix.co>
2015-04-15 21:14:31 +0530

    handled empty values in perf for aggregations

--- nocout_device_app/poller/ETL/performance/service/aggregation_all.py
+++ nocout_device_app/poller/ETL/performance/service/aggregation_all.py
@@ -80,18 +80,16 @@ def prepare_data(aggregated_data_values=[]):
     data_values = []
     end_time = datetime.now()
     start_time = end_time - timedelta(hours=hours)
-    start_time, end_time = start_time - timedelta(minutes=1), end_time + timedelta(minutes=1)
-    start_time, end_time = int(start_time.strftime('%s')), int(end_time.strftime('%s'))
+    start_time = start_time - timedelta(minutes=1)
+    print start_time, end_time
     if read_from == 'mysql':
+        start_time, end_time = int(start_time.strftime('%s')), int(end_time.strftime('%s'))
         db = mysql_migration_mod.mysql_conn(mysql_configs=mysql_configs)
         if db:
             # Read data from mysqldb, performance historical data
             data_values = mysql_migration_mod.read_data(source_perf_table, db, start_time, end_time)
             groupby_key = 'device_name'
     elif read_from == 'mongodb':
-        end_time = datetime.now()
-        start_time = end_time - timedelta(hours=hours)
-        start_time, end_time = start_time - timedelta(minutes=1), end_time + timedelta(minutes=1)
         # Read data from mongodb, performance live data
         data_values = mysql_migration_mod.read_data_from_mongo(source_perf_table, start_time, end_time, mongo_configs)
         groupby_key = 'host'
@@ -209,6 +207,17 @@ def quantify_perf_data(host_specific_data):
                 'max': current_value,
                 'avg': current_value,
             })
+	# do not process, if doc doesn't contain a valid value in max/min/avg field
+	if aggr_data.get('max') in (None, ''):
+	    continue
+	aggr_data.update({
+		'min': [aggr_data.get('min')],
+		'max': [aggr_data.get('max')],
+		'avg': [aggr_data.get('avg')],
+		})
+	#if ip_address == '10.160.32.32' and service in ['wimax_ss_dl_utilization', 'wimax_dl_intrf', 'wimax_dl_cinr']:
+	#	print '=====comming doc====='
+	#	print aggr_data
 
         # Find the existing doc to update
         find_query = {
@@ -222,57 +231,119 @@ def quantify_perf_data(host_specific_data):
         #print existing_doc, existing_doc_index
         if existing_doc:
             existing_doc = existing_doc[0]
-	    existing_doc = type_caste(existing_doc)
-            values_list = [existing_doc.get('max'), aggr_data.get('max'), existing_doc.get('min'), aggr_data.get('min')]
-	    values_list = [x for x in values_list if x != None]
-	    if not values_list:
-		values_list = [None]
+	    #existing_doc = type_caste(existing_doc)
+            #values_list = [existing_doc.get('max'), aggr_data.get('max'), existing_doc.get('min'), aggr_data.get('min')]
+	    #values_list = [x for x in values_list if x not in (None, '')]
+	    #if not values_list:
+	#	values_list = [None]
 	    # we calculate values for latency as 0 for pd = 100% cases
 	    # we need to remove those values for min, max calculations
-	    if str(ds) == 'rta':
-		values_list = [x for x in values_list if x != 0]
-		if not values_list:
-		    values_list = [0.0]
-            if service in wimax_mrotek_services or '_status' in service or '_invent' in service:
-                occur = collections.defaultdict(int)
-                for val in values_list:
-                    occur[val] += 1
-                freq_dist = occur.keys()
-                min_val = freq_dist[0]
-                max_val = freq_dist[-1]
-                avg_val = None
-            else:
-		if values_list:
-			min_val = min(values_list) 
-		if values_list:
-			max_val = max(values_list) 
-                if aggr_data.get('avg'):
-                    try:
-                        avg_val = (float(existing_doc.get('avg')) + float(aggr_data.get('avg')))/ 2.0
-                    except Exception:
-                        avg_val = existing_doc.get('avg') if existing_doc.get('avg') else aggr_data.get('avg')
-                else:
-                    avg_val = existing_doc.get('avg')
-            aggr_data.update({
-                'min': min_val,
-                'max': max_val,
-                'avg': avg_val
-                })
+	#    if str(ds) == 'rta':
+	#	values_list = [x for x in values_list if x != 0]
+	#	if not values_list:
+	#	    values_list = [0.0]
+        #    if service in wimax_mrotek_services or '_status' in service or '_invent' in service:
+        #        occur = collections.defaultdict(int)
+        #        for val in values_list:
+        #            occur[val] += 1
+        #        freq_dist = occur.keys()
+        #        min_val = freq_dist[0]
+        #        max_val = freq_dist[-1]
+        #        avg_val = None
+        #    else:
+	#	if values_list:
+	#		min_val = min(values_list) 
+	#	if values_list:
+	#		max_val = max(values_list) 
+        #        if aggr_data.get('avg') not in (None, ''):
+        #            try:
+        #                avg_val = (float(existing_doc.get('avg')) + float(aggr_data.get('avg')))/ 2.0
+        #            except Exception:
+        #                avg_val = existing_doc.get('avg') if existing_doc.get('avg') else aggr_data.get('avg')
+        #        else:
+        #            avg_val = existing_doc.get('avg')
+        #    aggr_data.update({
+        #        'min': min_val,
+        #        'max': max_val,
+        #        'avg': avg_val
+        #        })
+	    aggr_data['min'] += existing_doc['min']
+	    aggr_data['max'] += existing_doc['max']
+	    aggr_data['avg'] += existing_doc['avg']
             # First remove the existing entry from aggregated_data_values
             host_specific_aggregated_data.pop(existing_doc_index)
 	# round floats to 2 decimal places
 	# since we cant round to 2 decimal places in python <= 2.6, directly,
         # convert these values to str instead
-	try:
-            aggr_data['current_value'] = "{0:.2f}".format((float(aggr_data['current_value'])))
-            aggr_data['min'] = "{0:.2f}".format((float(aggr_data['min'])))
-            aggr_data['max'] = "{0:.2f}".format((float(aggr_data['max'])))
-            aggr_data['avg'] = "{0:.2f}".format((float(aggr_data['avg'])))
-        except:
-            # dont change any thing
-            pass
+	#try:
+        #    aggr_data['current_value'] = "{0:.2f}".format((float(aggr_data['current_value'])))
+        #    aggr_data['min'] = "{0:.2f}".format((float(aggr_data['min'])))
+        #    aggr_data['max'] = "{0:.2f}".format((float(aggr_data['max'])))
+        #    aggr_data['avg'] = "{0:.2f}".format((float(aggr_data['avg'])))
+        #except:
+        #    # dont change any thing
+        #    pass
+	#if ip_address == '10.160.32.32' and service in ['wimax_ss_dl_utilization', 'wimax_dl_intrf', 'wimax_dl_cinr']:
+	#	print '=====existing doc====='
+	#	print existing_doc
+	#	print '=====updated doc====='
+	#	print aggr_data
+	#	print '\n\n'
         host_specific_aggregated_data.append(aggr_data)
     return host_specific_aggregated_data
+
+
+def calc_values(data):
+    wimax_mrotek_services = ['wimax_ss_sector_id', 'wimax_ss_mac', 'wimax_dl_intrf', 'wimax_ul_intrf', 'wimax_ss_ip',
+                'wimax_modulation_dl_fec', 'wimax_modulation_ul_fec', 'wimax_ss_frequency',
+                'rici_line_1_port_state', 'rici_fe_port_state', 'rici_e1_interface_alarm',
+                'rici_device_type', 'mrotek_line_1_port_state', 'mrotek_fe_port_state',
+                'mrotek_e1_interface_alarm', 'mrotek_device_type']
+    for doc in data:
+	current_value = doc.get('current_value')
+	min_list = doc.get('min')
+	max_list = doc.get('max')
+	avg_list = doc.get('avg')
+	service = str(doc['service'])
+	if str(doc['ds']) == 'rta':
+            min_list = [x for x in min_list if x != 0]
+            if not min_list:
+                min_list = [0.0]
+            max_list = [x for x in max_list if x != 0]
+            if not max_list:
+                max_list = [0.0]
+            avg_list = [x for x in avg_list if x != 0]
+            if not avg_list:
+                avg_list = [0.0]
+	if service in wimax_mrotek_services or '_status' in service or '_invent' in service:
+                occur = collections.defaultdict(int)
+                for val in min_list:
+                    occur[val] += 1
+                min_val = min(occur, key=occur.get)
+
+                occur = collections.defaultdict(int)
+                for val in max_list:
+                    occur[val] += 1
+                max_val = max(occur, key=occur.get)
+		avg_val = None
+	else:
+	    min_val = min(min_list)
+	    max_val = max(max_list)
+	    avg_val = sum(avg_list)/float(len(avg_list))
+        try:
+            doc['current_value'] = "{0:.2f}".format((float(current_value)))
+            doc['min'] = "{0:.2f}".format((float(min_val)))
+            doc['max'] = "{0:.2f}".format((float(max_val)))
+            doc['avg'] = "{0:.2f}".format((float(avg_val)))
+        except:
+            doc['current_value'] = current_value
+            doc['min'] = min_val
+            doc['max'] = max_val
+            doc['avg'] = avg_val
+	#if doc['ip_address'] == '10.160.32.32' and service in ['wimax_ss_dl_utilization', 'wimax_dl_intrf', 'wimax_dl_cinr']:
+	#	print '=====Final doc====='
+	#	print doc
+    return data
 
 
 def type_caste(data):
@@ -305,7 +376,10 @@ def find_existing_entry(find_query, host_specific_aggregated_data):
     existing_doc_index = None
     find_values = set(find_query.values())
     for i in xrange(len(host_specific_aggregated_data)):
-        if find_values <= set(host_specific_aggregated_data[i].values()):
+	find_in = set([host_specific_aggregated_data[i].get('host'), 
+		host_specific_aggregated_data[i].get('ds'),
+		host_specific_aggregated_data[i].get('service')])
+        if find_values <= find_in:
             existing_doc = host_specific_aggregated_data[i:i+1]
             existing_doc_index = i
             break
@@ -319,8 +393,9 @@ def usage():
 
 if __name__ == '__main__':
     final_data_values = prepare_data()
-    if final_data_values:
+    final_aggr_data_values = calc_values(final_data_values)
+    if final_aggr_data_values:
 	db = mysql_migration_mod.mysql_conn(mysql_configs=mysql_configs)
-        mysql_migration_mod.mysql_export(destination_perf_table, db, final_data_values)
+        mysql_migration_mod.mysql_export(destination_perf_table, db, final_aggr_data_values)
     print 'Length of Data Inserted'
     print len(final_data_values)
