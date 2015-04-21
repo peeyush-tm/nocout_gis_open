09f72d47e8a4a3c671e83a1ee156f5394e389207
nitin <nitin@teramatrix.co>
2015-04-21 17:49:11 +0530

    #1410 Inventory services data missing for WAR/CRIT states

--- nocout_device_app/poller/ETL/performance/inventory/inventory_rrd_migration.py
+++ nocout_device_app/poller/ETL/performance/inventory/inventory_rrd_migration.py
@@ -116,7 +116,7 @@ def inventory_perf_data(site,hostlist,mongo_host,mongo_port,mongo_db_name):
 		host_ip = entry[1] 
 		service = entry[3]
 		try:				
-			plugin_output = str(entry[5].split('OK - ')[1])
+			plugin_output = str(entry[5].split('- ')[1])
 			plugin_output=plugin_output.strip()
 		except Exception as e:
 			print e
