1e7a96580b2867d8a270776b65d232f3dd772c63
nishant <nishant@teramatrix.co>
2015-04-28 18:34:10 +0530

    Excluded columns passes in query strin

--- nocout/nocout/static/js/utils/jqueryDataTable.js
+++ nocout/nocout/static/js/utils/jqueryDataTable.js
@@ -21,7 +21,8 @@ function ourDataTableWidget()
         header_class_name,
         data_class_name,
         header_extra_param,
-        data_extra_param
+        data_extra_param,
+        excluded_columns
     ) {
         /*Show the spinner*/
         showSpinner();
@@ -68,6 +69,7 @@ function ourDataTableWidget()
                                             data_class_name="'+data_class_name+'" \
                                             header_extra_param="'+header_extra_param+'" \
                                             data_extra_param="'+data_extra_param+'" \
+                                            excluded_columns="'+excluded_columns+'"\
                                             class="btn btn-sm btn-default" title="Download">\
                                             <i class="fa fa-download"></i></button>';
                     }
@@ -137,8 +139,15 @@ function ourDataTableWidget()
                     header_class_name = attributes_dict.header_class_name ? attributes_dict.header_class_name.value : "",
                     header_extra_param = attributes_dict.header_extra_param ? attributes_dict.header_extra_param.value : "",
                     data_extra_param = attributes_dict.data_extra_param ? attributes_dict.data_extra_param.value : "",
-                    url_get_param = "app="+app_name+"&rows="+data_class_name+"&headers="+header_class_name+"&headers_data="+header_extra_param+"&rows_data="+data_extra_param,
-                    download_url = main_url+""+url_get_param;
+                    excluded_columns = attributes_dict.excluded_columns ? attributes_dict.excluded_columns.value : "",
+                    url_get_param = "";
+
+                url_get_param += "app="+app_name;
+                url_get_param += "&rows="+data_class_name+"&rows_data="+data_extra_param;
+                url_get_param += "&headers="+header_class_name+"&headers_data="+header_extra_param;
+                url_get_param += "&excluded="+excluded_columns;
+
+                var download_url = main_url+""+url_get_param;
                 
                 if(!$("#"+tableId+"_download_btn").hasClass('disabled')) {
                     $("#"+tableId+"_download_btn").addClass('disabled');
