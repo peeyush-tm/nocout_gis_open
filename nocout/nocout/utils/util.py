46dfbb7c3d3c6ee6f04099c84791820f725b1384
Peeyush <praj@teramatrix.in>
2015-06-10 21:46:05 +0530

    Profile cache based functions without line or memory

--- nocout/nocout/utils/util.py
+++ nocout/nocout/utils/util.py
@@ -411,6 +411,8 @@ def time_it(debug=getattr(settings, 'PROFILE')):
                             show_results(profiler)
                         else:
                             pass
+                    else:
+                        pass
             else:
                 result = fn(*args, **kwargs)
             end = datetime.datetime.now()
@@ -588,17 +590,26 @@ def cache_for(time):
                     profile_type = getattr(settings, 'PROFILE_TYPE')
                     if profile_type == 'line':
                         profiler = LLP()
-                        profiled_func = profiler(fn)
-                    else:
+                    elif profile_type == 'memory':
                         profiler = MLP()
-                        profiled_func = profiler(fn)
+                    else:
+                        profiler = None
                     try:
-                        result = profiled_func(*args, **kwargs)
-                    finally:
-                        if profile_type == 'line':
-                            profiler.print_stats()
+                        if profiler:
+                            profiled_func = profiler(fn)
+                            result = profiled_func(*args, **kwargs)
                         else:
-                            show_results(profiler)
+                            result = fn(*args, **kwargs)
+                    finally:
+                        if profiler:
+                            if profile_type == 'line':
+                                profiler.print_stats()
+                            elif profile_type == 'memory':
+                                show_results(profiler)
+                            else:
+                                pass
+                        else:
+                            pass
                     cache.set(key, result, time)
                 else:
                     result = fn(*args, **kwargs)
