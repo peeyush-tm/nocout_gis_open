ce16167281ba34286a7593482b2ebe1586fe488f
mayank-tm <mayank.sharma@teramatrix.in>
2015-07-13 03:26:31 -0700

    Issue in Network Alert center sorting

--- nocout/alert_center/views.py
+++ nocout/alert_center/views.py
@@ -1201,7 +1201,7 @@ class GetNetworkAlertDetail(BaseDatatableView):
                 if sort_using in self.polled_value_columns:
                     qs = sorted(sort_data, key=lambda data: float(data[sort_using]), reverse=reverse)
                 else:
-                    qs = sorted(sort_data, key=itemgetter(sort_using), reverse=reverse)
+                    qs = sorted(sort_data, key=lambda data: data[sort_using].lower(), reverse=reverse)
             except Exception, e:
                 pass
         return qs
