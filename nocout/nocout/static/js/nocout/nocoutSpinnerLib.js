cd069e265108b37d6f2626d689d390423c1cc8a4
nishant <nishant@teramatrix.co>
2015-09-25 16:39:42 +0530

    Network alert details page too much tab GUI issue - resolved

--- nocout/nocout/static/js/nocout/nocoutSpinnerLib.js
+++ nocout/nocout/static/js/nocout/nocoutSpinnerLib.js
@@ -62,3 +62,115 @@ function createPaginateTabs() {
         // console.log($('.top_perf_tabs li').length);
     }
 }
+
+
+// Add fa-ellipsis-v icon before every tab text
+var page_tabs_obj = $('.nav-tabs li a');
+for (var i=0;i <page_tabs_obj.length;i++) {
+    var anchor_html = $(page_tabs_obj[i]).html();
+    if (anchor_html.indexOf('</i>') == -1) {
+        $(page_tabs_obj[i]).prepend('<i class="fa fa-ellipsis-v">&nbsp;</i>');
+    }
+}
+
+var tab_elem = 'ul.top_perf_tabs',
+    child_tag_name = 'li';
+
+
+/**
+ * This function show/hide paging functionality of tabs if they exceed from parent's width
+ * @method createTabsPaging
+ * @param elem {String}, It contains the element dom selector
+ * @param child_tag {String}, It contains the child tag name
+ */
+function createTabsPaging(elem, child_tag) {
+    tab_elem = elem;
+    child_tag_name = child_tag;
+    var width_object = getParentChildWidth(elem, child_tag);
+
+    if (width_object.self_width > width_object.parent_width) {
+        if($('.paging_arrow').hasClass('hide')) {
+            $('.paging_arrow').removeClass('hide');
+        }
+    } else {
+        $(elem).parent('.header-tabs').attr('style', 'width:100%;')
+    }
+}
+
+/**
+ * This function return given element with as per its child & its parent n super parent width
+ * @method getParentChildWidth
+ * @param elem {String}, It contains the element dom selector
+ * @param child_tag {String}, It contains the child tag name
+ */
+function getParentChildWidth(elem, child_tag) {
+    var self_width = 0;
+    
+    for (var i=0;i<$(elem + ' ' + child_tag).length;i++) {
+        self_width += $($(elem + ' ' + child_tag)[i]).width();
+    }
+
+    var width_obj = {
+            'self_width' : self_width,
+            'parent_width' : $(elem).parent().width(),
+            'super_parent_width' : $(elem).parent().parent().width()
+        };
+
+    return width_obj;
+}
+
+/**
+ * This event triggers when mouse over/out from tab paging arrows
+ * @event hover
+ */
+$('.paging_arrow').hover(
+    function() {
+        is_mouse_out = false;
+        paginateTab(this);
+    },
+    function() {
+        is_mouse_out = true;
+    }
+);
+
+/**
+ * This function moves tabs in 'right' or 'left' direction as per the given param
+ * @method paginateTab
+ * @param self {Object}, It contains the current object of hover event
+ */
+function paginateTab(self) {
+    var width_object = getParentChildWidth(tab_elem, child_tag_name);
+
+    if (tab_elem.indexOf('alert_detail_tabs') > -1) {
+        extra_spacing = 50;
+    } else {
+        extra_spacing = 20;
+    }
+
+    var width_diff = (width_object.self_width - width_object.parent_width) + (width_object.super_parent_width - width_object.parent_width) + extra_spacing,
+        margin_left = $.trim($(tab_elem)[0].style.marginLeft);
+    
+    if (margin_left.indexOf('px') > -1) {
+        margin_left = Number(margin_left.split('px')[0]);
+    }
+
+    if ($(self).hasClass('right_arrow')) {
+        var m_left = margin_left;
+        if (!isNaN(Number(String(margin_left).split('-')[1]))) {
+            m_left = Number(String(margin_left).split('-')[1]);
+        }
+        if (m_left < width_diff) {
+            $(tab_elem).css('margin-left', margin_left -10);
+        }
+    } else {
+        if (margin_left < width_diff && margin_left != 0) {
+            $(tab_elem).css('margin-left', margin_left + 10);
+        }
+    }
+
+    setTimeout(function() {
+        if (!is_mouse_out) {
+            paginateTab(self);
+        }
+    }, 100);
+}
