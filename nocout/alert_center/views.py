bb945bd31572736ac7116b0434758dc44efe4d28
nishant <nishant@teramatrix.co>
2016-01-29 11:49:16 +0530

    SNMP traps listing issue - resolved

--- nocout/alert_center/views.py
+++ nocout/alert_center/views.py
@@ -2068,24 +2068,26 @@ class SIAListingTable(BaseDatatableView, AdvanceFilteringMixin):
                 for tech_name in tech_name_list:                             
                     if tech_name in device_technology_dict:
                         tech_name_id.append(device_technology_dict.get(tech_name))
-                # print tech_name_id
+            
+            ip_address_list = list(Device.objects.filter(
+                device_technology__in=tech_name_id
+            ).values_list('ip_address', flat=True))
+
             if filter_condition:
                 query = "queryset = self.model.objects.filter( \
-                                {0}Q(ip_address__in=(Device.objects.filter(device_technology__in = {1}).values_list('ip_address'))), \
-                                ({2}) \
-                            ).using(TRAPS_DATABASE).values(*{3})".format(
+                                {0}Q(ip_address__in=ip_address_list), \
+                                ({1}) \
+                            ).using(TRAPS_DATABASE).values(*{2})".format(
                                 not_condition_sign,
-                                tech_name_id,
                                 filter_condition,
                                 model_columns
                             )
 
             else:
                 query = "queryset = self.model.objects.filter( \
-                        {0}Q(ip_address__in=(Device.objects.filter(device_technology__in = {1}).values_list('ip_address')))\
-                    ).using(TRAPS_DATABASE).values(*{2})".format(
+                        {0}Q(ip_address__in=ip_address_list)\
+                    ).using(TRAPS_DATABASE).values(*{1})".format(
                         not_condition_sign,
-                        tech_name_id,
                         model_columns
                     )                   
             
