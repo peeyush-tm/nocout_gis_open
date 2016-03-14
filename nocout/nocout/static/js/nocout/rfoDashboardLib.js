046157ea13b8810cef3b0fed3e23d7dfb842aef9
nishant <nishant@teramatrix.co>
2016-03-14 11:19:33 +0530

    RFO dashboard chart display issues - resolved

--- nocout/nocout/static/js/nocout/rfoDashboardLib.js
+++ nocout/nocout/static/js/nocout/rfoDashboardLib.js
@@ -4,6 +4,22 @@
  * @uses jQuery, Highcharts
  */
 
+var chart_colors_list = [
+        '#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', 
+        '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1',
+        '#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce', 
+        '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a',
+        '#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', 
+        '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92', '#d69e9f',
+        '#1c3066', '#0a3c5d', '#2f6494', '#2f70a6', '#2c5d8a',
+        '#2a5a85', '#1f21f2', '#1f5a78', '#1d4263', '#1c3f5e',
+        '#001fb7', '#0d31ff', '#0b2536', '#0000ff', '#f04f2d',
+        '#36939e', '#9e466b', '#453e5b', '#ffceb4', '#e0301e',
+        '#23c5e3', '#c5e323', '#a8f2c3', '#6fd6a5', '#ffceb4',
+        '#e1ac96', '#3c2e28', '#53856d', '#b24747', '#a5e79b',
+        '#77e180', '#488864', '#8e92a2', '#ff0033', '#9bc7ae',
+        '#528969', '#ffdeed', '#b296a2', '#fb6547', '#e58e7c'
+    ];
 
 /**
  * This event trigger when any filter control selectbox value changed
@@ -75,6 +91,10 @@ function updateFiltersContent(dataset, filter_name, filter_title) {
     }
 }
 
+/**
+ * This function initializes RFO dashboard. It calls function to poplates charts/tables.
+ * @method initRfoDashboard
+ */
 function initRfoDashboard() {
     var display_type = $('select[name="display_selector"]').val(),
         selected_month = $('select[name="month_selector"]').val(),
@@ -145,14 +165,22 @@ function initRfoDashboard() {
     }
 
     if (load_chart) {
-        loadRFOChart(summation_url,String(selected_month), selected_state, selected_city);
+        loadRFOColumnChart(summation_url,String(selected_month), selected_state, selected_city);
     }
 
     // Hide Loading Spinner
     hideSpinner();
 }
 
-function loadRFOChart(ajax_url, month, selected_state, selected_city) {
+/**
+ * This function fetch data & loads master cause code column chart
+ * @method loadRFOColumnChart
+ * @param ajax_url {String}, If contains the API url to fetch master cause code data
+ * @param month {Integer}, It contains epoch timestamp for selected month
+ * @param selected_state {String}, If contains the selected state value(if any)
+ * @param selected_city {String}, If contains the selected city value(if any)
+ */
+function loadRFOColumnChart(ajax_url, month, selected_state, selected_city) {
 
     $.ajax({
         url: ajax_url + '?month='+month+'&request_for_chart=1&state_name=' + selected_state + '&city_name='+ selected_city,
@@ -180,16 +208,18 @@ function loadRFOChart(ajax_url, month, selected_state, selected_city) {
                     })
                 }
 
+                // Initialize column chart for master cause code
                 $('#rfo_column_chart_container').highcharts({
                     chart: {
                         type: 'column'
                     },
+                    colors: chart_colors_list,
                     title: {
                         text: 'PB TT RFO Analysis ' + year_month_str,
                         align: 'left'
                     },
                     xAxis: {
-                        // categories: categories,
+                        categories: [''],
                         title: {
                             text: 'Master Cause Code'
                         }
@@ -201,6 +231,17 @@ function loadRFOChart(ajax_url, month, selected_state, selected_city) {
                         },
                         labels: {
                             overflow: 'justify'
+                        }
+                    },
+                    tooltip: {
+                        formatter: function () {
+                            var series_name = this.series.name ? $.trim(this.series.name) : "Value",
+                                tooltip_html = "";
+
+                            tooltip_html += '<ul><li><b>Master Cause Code</b></li><br/>';
+                            tooltip_html += '<li>'+series_name+' : '+this.point.y+'</li><br/></ul>';
+
+                            return tooltip_html;
                         }
                     },
                     legend: {
@@ -244,6 +285,7 @@ function loadRFOChart(ajax_url, month, selected_state, selected_city) {
                                         sub_data_url += '&state_name=' + selected_state;
                                         sub_data_url += '&city_name='+ selected_city
 
+                                        // Call "createFaultPieChart" to show pie chart in popup
                                         createFaultPieChart(sub_data_url, series_name);
                                     }
                                 }
@@ -267,10 +309,42 @@ function loadRFOChart(ajax_url, month, selected_state, selected_city) {
     });
 }
 
-function createFaultPieChart(sub_data_url, series_name) {
+/**
+ * This function fetch & load sub cause code pie chart as per the given params
+ * @method createFaultPieChart
+ * @param api_url {String}, It contains the url of api to fetch sub cause code data
+ * @param master_causecode_name {String}, It contains name of master code for which the sub cause code data is to be drawn
+ */
+function createFaultPieChart(api_url, master_causecode_name) {
+
+    var middle_legends = {
+            itemDistance : 15,
+            itemMarginBottom : 5,
+            borderColor : "#CCCCCC",
+            borderWidth : "1",
+            borderRadius : "8",
+            itemStyle: {
+                color: '#555555',
+                fontSize : '10px'
+            }
+        },
+        side_legends = {
+            itemDistance : 15,
+            itemMarginBottom : 5,
+            borderColor : "#CCCCCC",
+            borderWidth : "1",
+            borderRadius : "8",
+            itemStyle: {
+                color: '#555555',
+                fontSize : '10px'
+            },
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        };
 
     $.ajax({
-        url: sub_data_url,
+        url: api_url,
         type: 'GET',
         success: function(all_data_response) {
             if (typeof(all_data_response) == 'string') {
@@ -278,12 +352,13 @@ function createFaultPieChart(sub_data_url, series_name) {
             }
 
             if (all_data_response['result'] == 'ok') {
-                var pie_chart_data = [],
+                var data_list = all_data_response['aaData'],
+                    pie_chart_data = [],
                     pie_dict = {},
                     names_list = [];
-                for(var j=0; j<all_data_response['aaData'].length; j++) {
-                    var sub_causecode = all_data_response['aaData'][j]['sub_causecode'] ? all_data_response['aaData'][j]['sub_causecode'] : 'NA',
-                        outage_time = all_data_response['aaData'][j]['outage_in_minutes'] ? parseFloat(all_data_response['aaData'][j]['outage_in_minutes']) : 0;
+                for(var j=0; j<data_list.length; j++) {
+                    var sub_causecode = data_list[j]['sub_causecode'] ? data_list[j]['sub_causecode'] : 'NA',
+                        outage_time = data_list[j]['outage_in_minutes'] ? parseFloat(data_list[j]['outage_in_minutes']) : 0;
 
                     if (names_list.indexOf(sub_causecode) == -1) {
                         pie_dict[sub_causecode] = 0;
@@ -307,19 +382,25 @@ function createFaultPieChart(sub_data_url, series_name) {
 
                 bootbox.dialog({
                     message: bootbox_html,
-                    title: '<i class="fa fa-bar-chart">&nbsp;</i> Fault Segment - '+series_name
+                    title: '<i class="fa fa-bar-chart">&nbsp;</i> Fault Segment - '+master_causecode_name
                 });
 
                 // Update Modal width to 90%;
                 $(".modal-dialog").css("width","90%");
+                
+                var chart_legends = middle_legends;
+                // If more items in chart then show legends in right side(vertically)
+                if (pie_chart_data.length > 5) {
+                    chart_legends = side_legends;
+                }
 
                 $('#fault_pie_chart').highcharts({
                     chart: {
                         type: 'pie'
                     },
+                    colors: chart_colors_list,
                     title: {
-                        text: '',
-                        align: 'left'
+                        text: ''
                     },
                     xAxis: {
                         // categories: categories,
@@ -336,28 +417,35 @@ function createFaultPieChart(sub_data_url, series_name) {
                             overflow: 'justify'
                         }
                     },
-                    legend: {
-                        itemDistance : 15,
-                        itemMarginBottom : 5,
-                        borderColor : "#CCCCCC",
-                        borderWidth : "1",
-                        borderRadius : "8",
-                        itemStyle: {
-                            color: '#555555',
-                            fontSize : '10px'
-                        },
-                        layout: 'vertical',
-                        align: 'right',
-                        verticalAlign: 'top'
-                        // width : "100%"
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    },
+                    legend: chart_legends,
+                    tooltip: {
+                        formatter: function () {
+                            var point_name = this.key ? $.trim(this.key) : "Value",
+                                tooltip_html = "";
+
+                            tooltip_html += '<ul><li><b>'+master_causecode_name+'</b></li><br/>';
+                            tooltip_html += '<li>'+point_name+' : '+this.point.y+'</li><br/></ul>';
+
+                            return tooltip_html;
+                        }
                     },
                     credits: {
                         enabled: false
                     },
                     series: [{
                         data: pie_chart_data,
-                        name: series_name,
-                        title: series_name
+                        name: master_causecode_name,
+                        title: master_causecode_name
                     }],
                     noData: {
                         style: {
