5f832860872cd2422d75b94e30cec9ab5fbe4a0f
yogender <yogender@teramatrix.co>
2016-03-21 13:47:15 +0530

    sia & customer_count columns added to current, clear & history alarm tables

---
+++ nocout/alert_center/migrations/0008_auto_20160321_1310.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('alert_center', '0007_masteralarm'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='clearalarms',
+            name='customer_count',
+            field=models.IntegerField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='clearalarms',
+            name='sia',
+            field=models.CharField(max_length=32, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='currentalarms',
+            name='customer_count',
+            field=models.IntegerField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='currentalarms',
+            name='sia',
+            field=models.CharField(max_length=32, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='historyalarms',
+            name='customer_count',
+            field=models.IntegerField(null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='historyalarms',
+            name='sia',
+            field=models.CharField(max_length=32, null=True, blank=True),
+        ),
+    ]
