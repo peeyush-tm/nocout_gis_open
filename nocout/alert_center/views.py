30bd10df3a44d27838b6adaa465b498f168b85bf
achal-tm <achal.khunteta@teramatrix.in>
2016-10-18 14:26:22 +0530

    All Tab : Serverity icon color and Eventnames Updated

--- nocout/alert_center/views.py
+++ nocout/alert_center/views.py
@@ -2711,10 +2711,12 @@ class AllSiaListingTable(BaseDatatableView, AdvanceFilteringMixin):
 
     severity_icon_dict = {
         'latency': {
+            'warning': 'orange-dot',
             'major': 'red-dot',
             'clear': 'green-dot'
         },
         'packet_drop': {
+            'warning': 'orange-dot',
             'major': 'red-dot',
             'clear': 'green-dot'
         },
@@ -2787,9 +2789,9 @@ class AllSiaListingTable(BaseDatatableView, AdvanceFilteringMixin):
 
         # set filter condition according to the data source
         if data_source and data_source.lower() in ['packet_drop']:
-            eventname_list = ['PD_threshold_breach']
+            eventname_list = ['PD_threshold_breach_warning', 'PD_threshold_breach_major']
         elif data_source and data_source.lower() in ['latency']:
-            eventname_list = ['Latency_Threshold_Breach']
+            eventname_list = ['Latency_Threshold_Breach_warning', 'Latency_Threshold_Breach_major']
         elif data_source and data_source.lower() in ['down']:
             eventname_list = ['Device_not_reachable']
         else:
